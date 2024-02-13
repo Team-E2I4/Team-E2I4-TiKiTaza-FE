c6f9f51b9250e5a4edb86d418b3dd05ec9c80880
loevray <rei980213@gmail.com>
2024-02-13 16:34:54 +0900

    [🎨style]: 메인 페이지 우측 화면(검색, 방 만들기, 방 목록) grid 적용

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,15 +1,17 @@
 const MainPage = () => {
   return (
-    <main className='flex pb-[4rem]'>
+    <main className='flex pb-[4rem] gap-[3rem]'>
       <section className='flex flex-col gap-[3rem] w-[25rem]'>
         <div className='bg-yellow-100 h-[50rem] w-full'>접속자</div>
         <div className='bg-yellow-100 h-[4.5rem] w-full'>전체 랭킹 보기</div>
-        <div className='bg-yellow-100 h-[20.5rem] w-full'>유저 프로필</div>
+        <div className='bg-yellow-100 h-[18rem] w-full'>유저 프로필</div>
       </section>
-      <section>
+      <section className='flex-1 grid grid-cols-2 grid-rows-[5rem_auto] grid-flow-col gap-[3rem]'>
         <div className='bg-yellow-100'>검색 or 서버이름</div>
-        <div className='bg-yellow-100'>방 만들기</div>
-        <div className='bg-yellow-100'>방 목록</div>
+        <div className='bg-yellow-100 row-start-1'>방 만들기</div>
+        <div className='bg-yellow-100 row-start-2 col-start-1 col-span-2'>
+          방 목록
+        </div>
       </section>
     </main>
   );
