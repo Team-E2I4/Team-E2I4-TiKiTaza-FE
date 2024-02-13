fdf9873a955d295e3200ad82c486a8a7f214a94e
yejinleee <yejin9487@gmail.com>
2024-02-14 01:33:16 +0900

    [🎨style]: 인게임 헤더 수정, 인게임 랭킹 스타일

--- src/pages/GameSentencePage/GameSentencPage.tsx
+++ src/pages/GameSentencePage/GameSentencPage.tsx
@@ -1,15 +1,25 @@
 import { IngameHeader } from '@/common/Ingame/IngameHeader';
+import IngameRank from '@/common/Ingame/IngameRank';
 
 const GameSentencePage = () => {
   return (
     <>
       <IngameHeader
-        roomName='테스트방'
+        roomName='테스트방 이름이 길어진다면 1234567898765'
         participants={8}
         round={2}
         roundTotal={5}
       />
-      <div> game sentence page</div>
+      <IngameRank
+        isMe={false}
+        rank={2}
+        name='다른유저'
+      />
+      <IngameRank
+        isMe={true}
+        rank={3}
+        name='나야나'
+      />
     </>
   );
 };
