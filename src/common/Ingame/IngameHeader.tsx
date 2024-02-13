c369b90513bf58d965ad82eac6e586b6572bc0d6
yejinleee <yejin9487@gmail.com>
2024-02-13 21:35:49 +0900

    [🎨style]: IngameHeader 간격과 ellipsis 설정

--- src/common/Ingame/IngameHeader.tsx
+++ src/common/Ingame/IngameHeader.tsx
@@ -13,12 +13,14 @@ export const IngameHeader = ({
   roundTotal,
 }: IngameHeaderProps) => {
   return (
-    <div className='flex flex-row'>
+    <div className='flex flex-row items-center gap-20 pb-12'>
       <img
         src={backward}
         className='w-[4.8rem]'
       />
-      <div>{roomName}</div>
+      <div className='w-96 text-ellipsis overflow-hidden whitespace-nowrap'>
+        {roomName}이름이 길어진다면길게123456789
+      </div>
       <div className='grow'>참여 {participants}명</div>
       <div>
         🏁 {round} / {roundTotal}
