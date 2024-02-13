037742eea9e5dcd887e62c5e812d276c82eae625
loevray <rei980213@gmail.com>
2024-02-13 16:29:53 +0900

    [🎨style]: Header 높이 5rem으로 변경 및 MainPage레이아웃 기초 제작

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -1,5 +1,7 @@
 const Header = () => {
-  return <header className='bg-red-100 h-[10rem] w-[100%]'>나는 헤더</header>;
+  return (
+    <header className='bg-red-100 h-[5rem] w-[100%] shrink-0'>나는 헤더</header>
+  );
 };
 
 export default Header;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,18 @@
 const MainPage = () => {
-  return <div>main page</div>;
+  return (
+    <main className='flex pb-[4rem]'>
+      <section className='flex flex-col gap-[3rem] w-[25rem]'>
+        <div className='bg-yellow-100 h-[50rem] w-full'>접속자</div>
+        <div className='bg-yellow-100 h-[4.5rem] w-full'>전체 랭킹 보기</div>
+        <div className='bg-yellow-100 h-[20.5rem] w-full'>유저 프로필</div>
+      </section>
+      <section>
+        <div className='bg-yellow-100'>검색 or 서버이름</div>
+        <div className='bg-yellow-100'>방 만들기</div>
+        <div className='bg-yellow-100'>방 목록</div>
+      </section>
+    </main>
+  );
 };
 
 export default MainPage;
