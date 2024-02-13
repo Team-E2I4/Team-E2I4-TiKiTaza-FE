00c1b4f53d3b3f20c3b7c8a3682ce3b291302cc3
loevray <rei980213@gmail.com>
2024-02-13 16:55:56 +0900

    [🔨chore]: @radix-ui/react-avatar패키지 설치(아바타)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,5 @@
+import * as Avatar from '@radix-ui/react-avatar';
+
 const MainPage = () => {
   /* bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 클래스가 반복되어서 재사용 필요? */
   return (
@@ -11,6 +13,13 @@ const MainPage = () => {
         </div>
         <div className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[18rem] w-full'>
           유저 프로필
+          <Avatar.Root>
+            <Avatar.Image
+              className='size-[5rem] rounded-full'
+              src='https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80'
+              alt='프로필 이미지'
+            />
+          </Avatar.Root>
         </div>
       </section>
       <section className='flex-1 grid grid-cols-2 grid-rows-[5rem_auto] grid-flow-col gap-[3rem]'>
