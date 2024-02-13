af97074190011fbb432d31c6ef0c4e05062eeda0
yejinleee <yejin9487@gmail.com>
2024-02-13 20:21:03 +0900

    test

--- src/pages/GameSentencePage/GameSentencPage.tsx
+++ src/pages/GameSentencePage/GameSentencPage.tsx
@@ -1,5 +1,17 @@
+import { IngameHeader } from '@/common/Ingame/IngameHeader';
+
 const GameSentencePage = () => {
-  return <div> game sentence page</div>;
+  return (
+    <>
+      <IngameHeader
+        roomName='테스트방'
+        participants={8}
+        round={2}
+        roundTotal={5}
+      />
+      <div> game sentence page</div>
+    </>
+  );
 };
 
 export default GameSentencePage;
