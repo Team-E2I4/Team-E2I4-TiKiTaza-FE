a9f0e9f1ef608505aedfc3441a117a861d47ff18
loevray <rei980213@gmail.com>
2024-02-13 16:42:38 +0900

    [🎨style]: BodyLayout 배경색 beige-10으로 변경

--- src/common/Layout/BodyLayout.tsx
+++ src/common/Layout/BodyLayout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const BodyLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex-1 w-layout-w bg-green-100 px-[2rem] pt-[4rem]'>
+    <div className='flex-1 w-layout-w bg-beige-10 px-[2rem] pt-[4rem]'>
       {children}
     </div>
   );
