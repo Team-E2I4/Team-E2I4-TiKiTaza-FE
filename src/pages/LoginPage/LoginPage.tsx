55ef30e90deccacefe965a55f4c1a7a62f0acebd
Baek <woonn97@gmail.com>
2024-02-14 20:47:13 +0900

    [🎨style]: 카카오 버튼 CSS로 구현

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,5 @@
 import google from '@/asssets/login/g-logo.png';
-import kakao from '@/asssets/login/kakao_login.png';
+import kakao from '@/asssets/login/k-logo.png';
 import logo from '@/asssets/logo_big.png';
 
 const LoginPage = () => {
@@ -10,13 +10,15 @@ const LoginPage = () => {
         className='object-cover h-[55rem]'
       />
       <div className='flex flex-col gap-[0.5rem]'>
-        <div>
+        <div className='flex items-center gap-[8rem] bg-[#FEE500] w-[30rem] h-[4.5rem] rounded-[0.6rem]'>
           <img
             src={kakao}
-            className='w-[30rem]'
+            alt='Kakao logo'
+            className='w-[2rem] h-[2rem] ml-5'
           />
+          <span className='text-[1.4rem]'>카카오 로그인</span>
         </div>
-        <div className='flex items-center gap-[9rem] bg-white w-[30rem] h-[4.5rem] rounded-[0.6rem]'>
+        <div className='flex items-center gap-[9rem] bg-[#ffff] w-[30rem] h-[4.5rem] rounded-[0.6rem]'>
           <img
             src={google}
             alt='Google logo'
