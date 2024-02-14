2b1c0fe46088ee6976ff826bbf8e4f704c30f7a2
loevray <rei980213@gmail.com>
2024-02-14 12:01:08 +0900

    [🎨style]: 메인페이지 section 내부 div를 article태그로 변경

--- src/pages/MainPage/UserListItem.tsx
+++ src/pages/MainPage/UserListItem.tsx
@@ -3,7 +3,9 @@ interface UserListItemProps {
 }
 
 const UserListItem = ({ username }: UserListItemProps) => {
-  return <li className='truncate'>{username}</li>;
+  return (
+    <li className='truncate cursor-pointer hover:bg-green-100'>{username}</li>
+  );
 };
 
 export default UserListItem;
