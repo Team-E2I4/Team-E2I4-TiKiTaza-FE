32478ae2a0923432a6de8a6865853f23763a9b95
dlsxjzld <sinjuk1@naver.com>
2024-02-14 14:17:58 +0900

    [🎨style]: 진입 화면 UI 레이아웃 완성

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,5 +1,20 @@
+import logo_big from '@/asssets/logo_big.png';
+
 const StartPage = () => {
-  return <div></div>;
+  return (
+    <main className='w-full h-full flex flex-col justify-center items-center gap-[2rem]'>
+      <img
+        src={logo_big}
+        className='object-cover h-[55rem]'
+      />
+      <button className='w-[33.9rem] h-[8.8rem] text-[3.2rem] shadow-xl bg-green-100 border-solid border-2 border-black rounded-full'>
+        시작하기
+      </button>
+      <span className='font-bold text-[3.2rem]'>
+        당신의 손가락이 빠르고 정확한지 확인해보세요!
+      </span>
+    </main>
+  );
 };
 
 export default StartPage;
