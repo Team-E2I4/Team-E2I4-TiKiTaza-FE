b39a73b99905ec39e915e2a275b6c017207d3cff
loevray <rei980213@gmail.com>
2024-02-14 19:01:56 +0900

    [♻️refactor]: 게임 목록 부분 GameRoomList컴포넌트로 분리

---
+++ src/pages/MainPage/GameRoomList.tsx
@@ -0,0 +1,7 @@
+const GameRoomList = () => {
+  return (
+    <article className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 row-start-2 col-start-1 col-span-2'></article>
+  );
+};
+
+export default GameRoomList;
