c738023f308ca5009f823bc23a9340ba51b648db
yejinleee <yejin9487@gmail.com>
2024-02-14 20:11:56 +0900

    [🎨style]: 인게임 코드 highlight 패키지 사용

--- src/pages/GameCodePage/GameCodePage.tsx
+++ src/pages/GameCodePage/GameCodePage.tsx
@@ -1,5 +1,52 @@
+import Highlight from 'react-highlight';
+import { IngameHeader } from '@/common/Ingame/IngameHeader';
+import IngameRankContainer from '@/common/Ingame/IngameRank';
+import { SentenceInput } from '@/common/Ingame/SentenceInput';
+
+const GameContainer = () => {
+  const dummyCode = `function dfs(graph, start, visited) {
+    const stack = [];
+    stack.push(start);
+    while (stack.length) {
+      let v = stack.pop();
+      if (!visited[v]) {
+        console.log(v);
+        visited[v] = true;
+        for (let node of graph[v]) {
+          if (!visited[node]) {
+            stack.push(node);
+          }
+        }
+      }
+    }
+  }`;
+  return (
+    <>
+      <div className='w-[60rem]'>
+        <Highlight className='javascript'>{dummyCode}</Highlight>
+      </div>
+    </>
+  );
+};
 const GameCodePage = () => {
-  return <div>게임코드페이지</div>;
+  return (
+    <>
+      <IngameHeader />
+      <div>
+        <div className='absolute'>
+          <IngameRankContainer />
+        </div>
+        <div className='flex flex-col items-center justify-center ml-80 h-[50rem] border-2 border-black'>
+          <div className='flex'>
+            <div></div>
+            <GameContainer />
+            <div></div>
+          </div>
+          <SentenceInput />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default GameCodePage;
