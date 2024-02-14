00df3aae1875ed214c448ac0e8b981c3fd04a536
loevray <rei980213@gmail.com>
2024-02-14 13:38:04 +0900

    [🎨style]: 메인페이지 내 유저 프로필 영역 UserCard로 분리

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,4 @@
-import * as Avatar from '@radix-ui/react-avatar';
+import UserCard from './UserCard';
 import UserList from './UserList';
 
 const MainPage = () => {
@@ -10,25 +10,7 @@ const MainPage = () => {
         <article className='flex items-center justify-center bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[4.5rem] w-full'>
           <button>전체 랭킹 보기</button>
         </article>
-        <article className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[18rem] w-full p-[1.2rem]'>
-          <div className='flex justify-between'>
-            <span>닉네임</span>
-            <i>♣</i>
-          </div>
-          <div className='py-[2.2rem] flex'>
-            <Avatar.Root className='w-1/2'>
-              <Avatar.Image
-                className='size-[9rem] rounded-full'
-                src='https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80'
-                alt='프로필 이미지'
-              />
-              <Avatar.Fallback delayMs={1000}>테스트</Avatar.Fallback>
-            </Avatar.Root>
-            <div className='flex flex-col-reverse w-1/2'>
-              <span>무언가</span>
-            </div>
-          </div>
-        </article>
+        <UserCard />
       </section>
       <section className='flex-1 grid grid-cols-[3fr_1fr] grid-rows-[5rem_auto] grid-flow-col gap-[3rem]'>
         <article className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100'>
