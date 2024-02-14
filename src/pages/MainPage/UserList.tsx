8c21d8bf7ad2c2200dbff295f748938112dac4d7
loevray <rei980213@gmail.com>
2024-02-14 11:40:47 +0900

    [🔨chore]: eslint 변수명 규칙에 UPPER_CASE추가

--- src/pages/MainPage/UserList.tsx
+++ src/pages/MainPage/UserList.tsx
@@ -1,8 +1,30 @@
+import UserListItem from './UserListItem';
+
+const DUMMY_DATA = [
+  { username: '킴킴킴' },
+  { username: '아이린' },
+  { username: '킴킴킴' },
+  { username: '아이린' },
+  { username: '킴킴킴' },
+  { username: '아이린' },
+  { username: '킴킴킴' },
+  { username: '아이린' },
+  { username: '킴킴킴' },
+  { username: '아이린' },
+];
+
 const UserList = () => {
   return (
     <div className='flex flex-col p-[2rem] bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[50rem] w-full'>
       <h3>현재 접속자</h3>
-      <ul className='flex-1 rounded-[0.5rem] bg-green-50 p-[2rem]'></ul>
+      <ul className='flex-1 rounded-[0.5rem] bg-green-50 p-[2rem]'>
+        {DUMMY_DATA.map(({ username }, i) => (
+          <UserListItem
+            username={username}
+            key={i}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
