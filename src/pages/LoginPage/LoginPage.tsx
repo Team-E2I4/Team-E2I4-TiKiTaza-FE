0e634bd1713e1bf2ffe0ac5143aeb89bc9cd1aff
Baek <woonn97@gmail.com>
2024-02-14 19:58:16 +0900

    [🎨style]: 로그인 화면 UI 완성

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,32 @@
+import google from '@/asssets/login/g-logo.png';
+import kakao from '@/asssets/login/kakao_login.png';
+import logo from '@/asssets/logo_big.png';
+
 const LoginPage = () => {
-  return <div> login page</div>;
+  return (
+    <div className='flex flex-col items-center gap-[5rem]'>
+      <img
+        src={logo}
+        className='object-cover h-[55rem]'
+      />
+      <div className='flex flex-col gap-[0.5rem]'>
+        <div>
+          <img
+            src={kakao}
+            className='w-[30rem]'
+          />
+        </div>
+        <div className='flex items-center gap-[9rem] bg-white w-[30rem] h-[4.5rem] rounded-[0.6rem]'>
+          <img
+            src={google}
+            alt='Google logo'
+            className='w-[2rem] h-[2rem] ml-5'
+          />
+          <div className='text-[1.4rem]'>구글 로그인</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
