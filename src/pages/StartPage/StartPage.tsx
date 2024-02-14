ef79e6d88b9e0dc37d523c4dea15bb0145e2b4bb
dlsxjzld <sinjuk1@naver.com>
2024-02-14 15:02:31 +0900

    [🎨style]: asssets에 logo_big_shadow 추가, font 적용, 시작하기 border 제거

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,16 +1,17 @@
-import logo_big from '@/asssets/logo_big.png';
+import logo_big_shadow from '@/asssets/logo_big_shadow.png';
 
 const StartPage = () => {
   return (
     <main className='w-full h-full flex flex-col justify-center items-center gap-[2rem]'>
       <img
-        src={logo_big}
+        alt='티키타자 로고'
+        src={logo_big_shadow}
         className='object-cover h-[55rem]'
       />
-      <button className='w-[33.9rem] h-[8.8rem] text-[3.2rem] shadow-xl bg-green-100 border-solid border-2 border-black rounded-full'>
+      <button className='w-[33.9rem] h-[8.8rem] font-[Giants-Inline] text-[3.2rem] shadow-xl bg-green-100 rounded-full'>
         시작하기
       </button>
-      <span className='font-bold text-[3.2rem]'>
+      <span className='font-bold font-[Giants-Inline] text-[3.2rem]'>
         당신의 손가락이 빠르고 정확한지 확인해보세요!
       </span>
     </main>
