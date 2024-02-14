b4a92fcce020ec188d178c53b266e86f2174d76d
loevray <rei980213@gmail.com>
2024-02-14 11:33:31 +0900

    [🎨style]: UserList, UserListItem 컴포넌트 생성

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,12 @@
 import * as Avatar from '@radix-ui/react-avatar';
+import UserList from './UserList';
 
 const MainPage = () => {
   /* bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 클래스가 반복되어서 재사용 필요? */
   return (
     <main className='flex pb-[4rem] gap-[3rem]'>
       <section className='flex flex-col gap-[3rem] w-[25rem]'>
-        <div className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[50rem] w-full'>
-          접속자
-        </div>
+        <UserList />
         <div className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[4.5rem] w-full'>
           전체 랭킹 보기
         </div>
