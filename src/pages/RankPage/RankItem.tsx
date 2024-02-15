5e2ca1b399fd0553ec8e538849548a90ba8760e7
Baek <woonn97@gmail.com>
2024-02-15 14:35:42 +0900

    [♻️refactor]: radix-ui/react-tabs로 리팩토링

--- src/pages/RankPage/RankItem.tsx
+++ src/pages/RankPage/RankItem.tsx
@@ -1,3 +1,7 @@
+import first from '@/asssets/rank/first_rank.png';
+import second from '@/asssets/rank/second_rank.png';
+import third from '@/asssets/rank/third_rank.png';
+
 interface RankItemProps {
   rank: {
     nickname: string;
@@ -9,14 +13,28 @@ interface RankItemProps {
 const RankItem = ({ rank, index }: RankItemProps) => {
   return (
     <div className='flex gap-2 justify-center items-center font-bold text-4xl py-6 px-4 border-b border-gray-100'>
-      <span className='w-[30%] text-center'>
-        {index === 0
-          ? '🥇'
-          : index === 1
-            ? '🥈'
-            : index === 2
-              ? '🥉'
-              : index + 1}
+      <span className='w-[30%] flex justify-center items-center'>
+        {index === 0 ? (
+          <img
+            src={first}
+            alt='first'
+            className='h-10'
+          />
+        ) : index === 1 ? (
+          <img
+            src={second}
+            alt='second'
+            className='h-10'
+          />
+        ) : index === 2 ? (
+          <img
+            src={third}
+            alt='third'
+            className='h-10'
+          />
+        ) : (
+          index + 1
+        )}
       </span>
       <span className='w-[40%] text-center'>{rank.nickname}</span>
       <span className='w-[30%] text-center'>{rank.score}</span>
--- src/pages/RankPage/RankPage.tsx
+++ src/pages/RankPage/RankPage.tsx
@@ -1,16 +1,94 @@
+import * as Tabs from '@radix-ui/react-tabs';
 import RankList from './RankList';
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    nickname: 'user1',
+    score: 100,
+  },
+  {
+    id: 2,
+    nickname: 'user2',
+    score: 90,
+  },
+  {
+    id: 3,
+    nickname: 'user3',
+    score: 80,
+  },
+  {
+    id: 4,
+    nickname: 'user4',
+    score: 70,
+  },
+  {
+    id: 5,
+    nickname: 'user5',
+    score: 60,
+  },
+  {
+    id: 6,
+    nickname: 'user6',
+    score: 50,
+  },
+  {
+    id: 7,
+    nickname: 'user7',
+    score: 40,
+  },
+  {
+    id: 8,
+    nickname: 'user8',
+    score: 30,
+  },
+  {
+    id: 9,
+    nickname: 'user9',
+    score: 20,
+  },
+];
 
 const RankPage = () => {
   return (
-    <div className='flex flex-col gap-[4rem] w-[60%] mx-auto'>
-      <div className='flex gap-[12rem] justify-center font-bold font-[Giants-Inline] text-4xl'>
-        <div className='bg-coral-100 rounded-[1rem] px-6 py-3'>전체</div>
-        <div className='bg-coral-100 rounded-[1rem] px-6 py-3'>코드</div>
-        <div className='bg-coral-100 rounded-[1rem] px-6 py-3'>문장</div>
-        <div className='bg-coral-100 rounded-[1rem] px-6 py-3'>단어</div>
-      </div>
-      <RankList />
-    </div>
+    <Tabs.Root
+      className='flex flex-col gap-[4rem] w-[60%] mx-auto'
+      defaultValue='tab1'>
+      <Tabs.List className='flex gap-[11rem] justify-center font-bold font-[Giants-Inline] text-4xl text-white'>
+        <Tabs.Trigger
+          className='bg-coral-100 rounded-[1rem] px-8 py-4'
+          value='tab1'>
+          전체
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          className='bg-coral-100 rounded-[1rem] px-8 py-4'
+          value='tab2'>
+          단어
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          className='bg-coral-100 rounded-[1rem] px-8 py-4'
+          value='tab3'>
+          문장
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          className='bg-coral-100 rounded-[1rem] px-8 py-4'
+          value='tab4'>
+          코드
+        </Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value='tab1'>
+        <RankList data={DUMMY_DATA} />
+      </Tabs.Content>
+      <Tabs.Content value='tab2'>
+        <RankList data={DUMMY_DATA} />
+      </Tabs.Content>
+      <Tabs.Content value='tab3'>
+        <RankList data={DUMMY_DATA} />
+      </Tabs.Content>
+      <Tabs.Content value='tab4'>
+        <RankList data={DUMMY_DATA} />
+      </Tabs.Content>
+    </Tabs.Root>
   );
 };
 
