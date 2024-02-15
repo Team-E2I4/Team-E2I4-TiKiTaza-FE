221b6c4ffd7558b986c6858062bbab168eecc280
loevray <rei980213@gmail.com>
2024-02-15 12:03:32 +0900

    [✨feat]: Divider컴포넌트 추가. 수직-수평에 대응합니다

--- src/common/Divider/Divider.tsx
+++ src/common/Divider/Divider.tsx
@@ -1,4 +1,19 @@
-const Divider = () => {
-  return <div className='flex w-full h-[0.1rem] bg-gray-100 my-[0.5rem]'></div>;
+import { ComponentProps } from 'react';
+
+interface DividerProps {
+  orientation?: 'horizontal' | 'vertical';
+  className?: ComponentProps<'div'>['className'];
+}
+
+const Divider = ({ orientation = 'horizontal', className }: DividerProps) => {
+  const orientationStyle = new Map([
+    ['horizontal', 'h-[0.1rem]'],
+    ['vertical', 'w-[0.1rem]'],
+  ]);
+  return (
+    <div
+      className={`bg-blue-100 ${className} ${orientationStyle.get(orientation)}`}></div>
+  );
 };
+
 export default Divider;
