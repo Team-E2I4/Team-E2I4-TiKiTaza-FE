051365969720c20eaafc6e0cb4ba5a171342caec
loevray <rei980213@gmail.com>
2024-02-15 14:22:30 +0900

    [🎨style]: 자물쇠 위치 조정 및 방 제목 최대크기 조절

--- src/pages/MainPage/GameRoonListItem.tsx
+++ src/pages/MainPage/GameRoonListItem.tsx
@@ -26,20 +26,16 @@ const GameRoomListItem = ({
     <li className='flex w-full py-[1rem] bg-gray-10 hover:bg-coral-50 cursor-pointer'>
       <span className='text-center truncate flex-1'>{`No.${roomNumber}`}</span>
       <span>ㅣ</span>
-      <span className='text-center truncate flex-[4_0_0] flex'>
-        {isLocked ? (
-          <>
-            <img
-              src={locked}
-              alt='비공개'
-              className='mx-[1rem]'
-            />
-            <span className='truncate'>{title}</span>
-          </>
-        ) : (
-          <span className='truncate'>{title}</span>
+      <div className='flex-[4_0_0] truncate flex justify-center'>
+        {isLocked && (
+          <img
+            src={locked}
+            alt='비공개'
+            className='px-[1rem]'
+          />
         )}
-      </span>
+        <span className='text-center truncate max-w-[60%]'>{title}</span>
+      </div>
       <span>ㅣ</span>
       <span className='text-center truncate flex-1'>{MODE_TYPE[mode]}</span>
       <span>ㅣ</span>
