821f4d75f9ff3d9059fe98ad8f8b399257dedced
yejinleee <yejin9487@gmail.com>
2024-02-15 14:55:58 +0900

    [🎨style]: 인게임 단어 자동차 랭킹 ui 구현

--- src/pages/GameWordPage/GameWordPage.tsx
+++ src/pages/GameWordPage/GameWordPage.tsx
@@ -1,8 +1,7 @@
 import { ReactNode } from 'react';
 import IngameHeader from '@/common/Ingame/IngameHeader';
-import IngameRank from '@/common/Ingame/IngameRank';
 import Input from '@/common/Input/Input';
-import { wordDummy } from './wordDummy';
+import { wordDummy, wordRankDummy } from './wordDummy';
 
 const positions = ['center', 'left', 'right'];
 const WordCell = ({ children }: { children: ReactNode }) => {
@@ -19,28 +18,60 @@ function shuffle(array: string[]) {
   return array.sort(() => Math.random() - 0.5);
 }
 const words = shuffle(wordDummy.concat(Array(20).fill('')));
-
+interface WordRankProps {
+  userId: number;
+  track: number;
+  userName: string;
+  score: number;
+}
+const WordRank = (data: WordRankProps) => {
+  return (
+    <>
+      <div className='w-24 box-content relative'>
+        <div
+          className={`border-black border-dashed border-r-2 h-[21rem] ${data.track === 0 && 'border-l-2 border-solid'}`}>
+          <span className='absolute bottom-12 left-8'>🚗</span>
+        </div>
+        <div
+          className={`flex justify-center pt-[0.5rem] ${data.userId === MYUSERID ? 'text-[1.8rem] text-green-100' : 'text-[1.4rem] text-gray-200'}`}>
+          {data.userName}
+        </div>
+      </div>
+    </>
+  );
+};
+const MYUSERID = 3;
 const GameWordPage = () => {
   return (
     <>
       <IngameHeader />
       <div className='grow'>
-        <div className='absolute'>
-          <IngameRank />
-        </div>
-        <div className='flex flex-col items-center justify-center ml-80 h-[60rem] border-2 border-black'>
+        <div className='flex flex-col items-center justify-around h-[60rem] border-2 border-black'>
           <div className='h-[25rem] grid grid-rows-[repeat(8,minmax(0,1fr))] grid-cols-[repeat(15,7rem)] text-[1.4rem]'>
             {words.map((w, i) => {
               return <WordCell key={i + w}>{w}</WordCell>;
             })}
           </div>
-          <div className='flex flex-row h-[30rem] bg-slate-200 mt£-4'>
-            <div>left</div>
-            <div>
-              <div>cars</div>
+          <div className='flex flex-row w-10/12 h-[30rem] mt-4'>
+            <div className='w-[20rem]'></div>
+            <div className='flex flex-col grow items-center'>
+              <div className='grow flex relative w-full justify-center'>
+                {wordRankDummy.map((rank, i) => {
+                  const { userId, userName, score } = rank;
+                  return (
+                    <WordRank
+                      key={i}
+                      track={i}
+                      userId={userId}
+                      userName={userName}
+                      score={score}
+                    />
+                  );
+                })}
+              </div>
               <Input style={`w-[20rem] h-[4rem]`} />
             </div>
-            <div>right</div>
+            <div className='w-[20rem]'></div>
           </div>
         </div>
       </div>
