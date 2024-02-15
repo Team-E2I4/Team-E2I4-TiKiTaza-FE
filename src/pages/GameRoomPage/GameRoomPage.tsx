76bbe1ade92ca11a1d22e46ac6736f8dc8bfcbb4
dlsxjzld <sinjuk1@naver.com>
2024-02-15 19:05:14 +0900

    [🎨style]: gameRoom에 참여한 UserCard UI 작업 완료 - Avatar 추가, UserCard layout 완료

--- src/pages/GameRoomPage/GameRoomPage.tsx
+++ src/pages/GameRoomPage/GameRoomPage.tsx
@@ -1,3 +1,4 @@
+import * as Avatar from '@radix-ui/react-avatar';
 import GameRoomHeader from './GameRoomHeader';
 
 interface I_gameRoomUserList {
@@ -83,11 +84,30 @@ const GameRoomPage = (props: GameRoomPageProps) => {
         {gameRoomUserList.map((gameRoomUser) => (
           <div
             key={gameRoomUser.userName}
-            className='w-[25rem] h-[21rem] flex flex-col bg-white shadow-md shadow-black/50 rounded-[2.5rem]'>
-            <button>❌</button>
-            {/* TODO: Avatar 추가 */}
-            <div>{gameRoomUser.userName}</div>
-            <div>{gameRoomUser.rank}등</div>
+            className='w-[25.8rem] h-[21.2rem] flex flex-col bg-white shadow-md shadow-black/50 rounded-[2.5rem]'>
+            <button className='self-end pt-[1.6rem] pr-[2.7rem] text-[1.6rem]'>
+              ❌
+            </button>
+            <div className='px-[2rem] py-[1rem] flex gap-[2rem]'>
+              <Avatar.Root className='w-1/2 self-center'>
+                <Avatar.Image
+                  className='size-[10rem] rounded-full'
+                  src={gameRoomUser.userImage}
+                  alt='프로필 이미지'
+                />
+                <Avatar.Fallback delayMs={gameRoomUser.userImageFallbackDelay}>
+                  test
+                </Avatar.Fallback>
+              </Avatar.Root>
+              <div className='w-1/2 flex flex-col items-center justify-around text-center'>
+                <div className='w-[10rem] py-[0.4rem] flex items-center justify-center bg-green-70 rounded-[2.5rem] '>
+                  {gameRoomUser.userName}
+                </div>
+                <div className='w-[10rem] py-[0.4rem] flex items-center justify-center bg-green-70 rounded-[2.5rem]'>
+                  {gameRoomUser.rank}등
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </main>
