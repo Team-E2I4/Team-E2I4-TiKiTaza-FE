b144cce47ba1e54ac65498a2dd8eb49c7c0ba9b9
Baek <woonn97@gmail.com>
2024-02-15 10:28:32 +0900

    [✨feat]: rank에 더미데이터 추가

--- src/pages/RankPage/RankPage.tsx
+++ src/pages/RankPage/RankPage.tsx
@@ -1,5 +1,64 @@
+import RankItem from './RankItem';
+
 const RankPage = () => {
-  return <div>rank page</div>;
+  const DUMMY_DATA = [
+    {
+      id: 1,
+      nickname: 'user1',
+      score: 100,
+    },
+    {
+      id: 2,
+      nickname: 'user2',
+      score: 90,
+    },
+    {
+      id: 3,
+      nickname: 'user3',
+      score: 80,
+    },
+    {
+      id: 4,
+      nickname: 'user4',
+      score: 70,
+    },
+    {
+      id: 5,
+      nickname: 'user3',
+      score: 60,
+    },
+    {
+      id: 6,
+      nickname: 'user3',
+      score: 50,
+    },
+    {
+      id: 7,
+      nickname: 'user3',
+      score: 40,
+    },
+    {
+      id: 8,
+      nickname: 'user3',
+      score: 30,
+    },
+    {
+      id: 9,
+      nickname: 'user3',
+      score: 20,
+    },
+  ];
+  return (
+    <div>
+      {DUMMY_DATA.map((rank, index) => (
+        <RankItem
+          key={rank.id}
+          rank={rank}
+          index={index}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default RankPage;
