003d65749eaf5b8ca3e1fe2021797b882b4dd520
yejinleee <yejin9487@gmail.com>
2024-02-15 11:31:08 +0900

    [🎨style]: Input 컴포넌트 props로 가로세로 길이 설정

--- src/common/Input/Input.tsx
+++ src/common/Input/Input.tsx
@@ -1,10 +1,13 @@
-const Input = () => {
+interface InputProps {
+  style?: string;
+}
+const Input = ({ style = `w-[70rem] h-[4.5rem]` }: InputProps) => {
   return (
     <>
       <input
-        className='w-[70rem] h-[4.5rem] flex items-center pl-[1.75rem] rounded-2xl
-       bg-white border-2 border-green-100 my-4
-       outline-0 text-gray-300 tracking-wider box-border'
+        className={`${style} flex items-center pl-[1.75rem] rounded-2xl
+bg-white border-2 border-green-100 my-4
+outline-0 text-gray-300 tracking-wider box-border`}
       />
     </>
   );
