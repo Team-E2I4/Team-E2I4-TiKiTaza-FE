7d0310b1c428e54a81f1094686a6631c850b5da0
Baek <woonn97@gmail.com>
2024-02-15 13:01:43 +0900

    [🎨style]: 랭킹 UI 수정

--- src/pages/RankPage/RankPage.tsx
+++ src/pages/RankPage/RankPage.tsx
@@ -2,11 +2,14 @@ import RankList from './RankList';
 
 const RankPage = () => {
   return (
-    <div>
-      <div></div>
-      <div>
-        <RankList />
+    <div className='flex flex-col gap-[4rem] w-[60%] mx-auto'>
+      <div className='flex gap-[12rem] justify-center font-bold font-[Giants-Inline] text-4xl'>
+        <div className='bg-coral-100 rounded-[1rem] px-6 py-3'>전체</div>
+        <div className='bg-coral-100 rounded-[1rem] px-6 py-3'>코드</div>
+        <div className='bg-coral-100 rounded-[1rem] px-6 py-3'>문장</div>
+        <div className='bg-coral-100 rounded-[1rem] px-6 py-3'>단어</div>
       </div>
+      <RankList />
     </div>
   );
 };
