6e9f7e3fcaad0ed7a21ae9f721cbc7f77477a800
loevray <rei980213@gmail.com>
2024-02-16 13:52:26 +0900

    [🎨style]: 소리관련 버튼 svg추가 및 유저용 임시사진 추가(강아지)

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -1,6 +1,39 @@
+import * as Avatar from '@radix-ui/react-avatar';
+import bgm_volume from '@/asssets/bgm_volume.svg';
+import effect_volume from '@/asssets/effect_volume.svg';
+import logo_car from '@/asssets/logo_car.png';
+import logo_taza from '@/asssets/logo_taza.png';
+
 const Header = () => {
   return (
-    <header className='bg-red-100 h-[5rem] w-[100%] shrink-0'>나는 헤더</header>
+    <header className='bg-green-100 h-[4.5rem] w-[100%] shrink-0 flex justify-between px-[4rem]'>
+      <section className='h-full flex items-center'>
+        <img
+          src={logo_car}
+          className='h-[70%]'
+        />
+        <img
+          src={logo_taza}
+          className='h-1/2'
+        />
+      </section>
+      <section className='h-full flex items-center justify-around w-[15rem]'>
+        <button>
+          <img src={bgm_volume} />
+        </button>
+        <button>
+          <img src={effect_volume} />
+        </button>
+        <Avatar.Root className='cursor-pointer'>
+          <Avatar.Image
+            className='size-[3.5rem] rounded-full'
+            src='https://picsum.photos/id/237/200/300'
+            alt='프로필 이미지'
+          />
+          <Avatar.Fallback delayMs={1000}>프로필</Avatar.Fallback>
+        </Avatar.Root>
+      </section>
+    </header>
   );
 };
 
