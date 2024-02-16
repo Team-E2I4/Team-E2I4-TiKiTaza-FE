1c9a34245b1f8975abbbde7635b54507f19a0685
Baek <woonn97@gmail.com>
2024-02-16 13:16:44 +0900

    [🎨style]: 화면 레이아웃 수정

--- src/pages/GameFinishPage/GameFinishPage.tsx
+++ src/pages/GameFinishPage/GameFinishPage.tsx
@@ -9,15 +9,15 @@ const GameFinishPage = () => {
           3초 뒤 자동으로 로비로 이동합니다...
         </span>
       </section>
-      <section className='flex gap-[10rem]'>
+      <section className='flex gap-[10rem] w-full'>
         <div className='flex flex-1 justify-center items-end'>
-          <div className='bg-[#FFF4DC] h-[21rem] w-[12rem] flex items-center justify-center shadow text-5xl font-bold'>
+          <div className='bg-[#FFF4DC] h-[28rem] w-[14rem] flex items-center justify-center shadow text-5xl font-bold'>
             2
           </div>
-          <div className='bg-[#FFF4DC] h-[30rem] w-[12rem] flex items-center justify-center shadow text-5xl font-bold'>
+          <div className='bg-[#FFF4DC] h-[40rem] w-[14rem] flex items-center justify-center shadow text-5xl font-bold'>
             1
           </div>
-          <div className='bg-[#FFF4DC] h-[16rem] w-[12rem] flex items-center justify-center shadow text-5xl font-bold'>
+          <div className='bg-[#FFF4DC] h-[20rem] w-[14rem] flex items-center justify-center shadow text-5xl font-bold'>
             3
           </div>
         </div>
@@ -26,10 +26,10 @@ const GameFinishPage = () => {
         </div>
       </section>
       <section className='flex gap-10 font-[Giants-Inline]'>
-        <div className='bg-coral-100 px-6 py-6 rounded-[12px] text-3xl font-bold'>
+        <div className='bg-coral-100 px-6 py-6 rounded-[1rem] text-3xl font-bold'>
           로비로 나가기
         </div>
-        <div className='bg-coral-100 px-6 py-6 rounded-[12px] text-3xl font-bold'>
+        <div className='bg-coral-100 px-6 py-6 rounded-[1rem] text-3xl font-bold'>
           다시하기
         </div>
       </section>
