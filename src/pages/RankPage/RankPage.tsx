415153401a51b218a3d6f30070dedf1df7527163
Baek <woonn97@gmail.com>
2024-02-16 11:36:16 +0900

    [🚚change]: 더미 데이터 파일 추가

--- src/pages/RankPage/RankPage.tsx
+++ src/pages/RankPage/RankPage.tsx
@@ -1,53 +1,6 @@
 import * as Tabs from '@radix-ui/react-tabs';
+import { DUMMY_DATA } from './RankData';
 import RankList from './RankList';
-
-const DUMMY_DATA = [
-  {
-    id: 1,
-    nickname: 'user1',
-    score: 100,
-  },
-  {
-    id: 2,
-    nickname: 'user2',
-    score: 90,
-  },
-  {
-    id: 3,
-    nickname: 'user3',
-    score: 80,
-  },
-  {
-    id: 4,
-    nickname: 'user4',
-    score: 70,
-  },
-  {
-    id: 5,
-    nickname: 'user5',
-    score: 60,
-  },
-  {
-    id: 6,
-    nickname: 'user6',
-    score: 50,
-  },
-  {
-    id: 7,
-    nickname: 'user7',
-    score: 40,
-  },
-  {
-    id: 8,
-    nickname: 'user8',
-    score: 30,
-  },
-  {
-    id: 9,
-    nickname: 'user9',
-    score: 20,
-  },
-];
 
 const RankPage = () => {
   const tabData = [
