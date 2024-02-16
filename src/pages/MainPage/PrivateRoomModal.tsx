f5dee17f203b65b4eec96223228c3d773af0b3f6
loevray <rei980213@gmail.com>
2024-02-16 22:45:17 +0900

    [🎨style]: 모달 기본스타일 추가

--- src/pages/MainPage/PrivateRoomModal.tsx
+++ src/pages/MainPage/PrivateRoomModal.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { ComponentProps, ReactNode } from 'react';
+import Input from '@/common/Input/Input';
 
 interface PrivateRoomModalProps {
   children: ReactNode;
@@ -17,15 +18,17 @@ const PrivateRoomModal = ({ children, className }: PrivateRoomModalProps) => {
           }
         />
         <Dialog.Content
-          className={
-            className
-              ? className
-              : 'bg-blue-100 z-10 fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'
-          }>
-          <Dialog.Title>Edit profile</Dialog.Title>
-          <Dialog.Description>하이</Dialog.Description>
+          className={`rounded-[1rem] border-green-100 border-[0.3rem] w-[30rem] h-[15rem] flex flex-col items-center justify-center bg-white z-10 fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] ${className}`}>
+          <Dialog.Title>비밀번호를 입력해주세요!</Dialog.Title>
+          <Dialog.Description>
+            <Input whSize='w-[24rem] h-[3rem]' />
+          </Dialog.Description>
           <Dialog.Close asChild>
-            <button aria-label='Close'>닫기2</button>
+            <button
+              aria-label='Close'
+              className='hover:bg-gray-100'>
+              [x]
+            </button>
           </Dialog.Close>
         </Dialog.Content>
       </Dialog.Portal>
