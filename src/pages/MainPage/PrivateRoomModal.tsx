7fdf80bc32e20631bb0c991e5e1d0494cf1a7e26
loevray <rei980213@gmail.com>
2024-02-16 22:39:53 +0900

    [🎨style]: 모달 기본 배경스타일 추가

--- src/pages/MainPage/PrivateRoomModal.tsx
+++ src/pages/MainPage/PrivateRoomModal.tsx
@@ -1,19 +1,29 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import { ComponentProps, ReactNode } from 'react';
 
-const PrivateRoomModal = () => {
+interface PrivateRoomModalProps {
+  children: ReactNode;
+  className?: ComponentProps<'div'>['className'];
+}
+
+const PrivateRoomModal = ({ children, className }: PrivateRoomModalProps) => {
   return (
     <Dialog.Root>
-      <Dialog.Trigger asChild>
-        <button>열기</button>
-      </Dialog.Trigger>
+      <Dialog.Trigger>{children}</Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay />
-        <Dialog.Content>
+        <Dialog.Overlay
+          className={
+            'fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-[100vw] h-[100vh] bg-black/30'
+          }
+        />
+        <Dialog.Content
+          className={
+            className
+              ? className
+              : 'bg-blue-100 z-10 fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'
+          }>
           <Dialog.Title>Edit profile</Dialog.Title>
           <Dialog.Description>하이</Dialog.Description>
-          <Dialog.Close asChild>
-            <button>닫기1</button>
-          </Dialog.Close>
           <Dialog.Close asChild>
             <button aria-label='Close'>닫기2</button>
           </Dialog.Close>
