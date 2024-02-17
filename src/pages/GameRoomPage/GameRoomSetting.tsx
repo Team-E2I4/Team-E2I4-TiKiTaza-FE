75de9f0965adcbd712cb1134a022a16472fff872
dlsxjzld <sinjuk1@naver.com>
2024-02-17 16:22:29 +0900

    [♻️refactor]: GameRoomFooter 컴포넌트 상세하게 분리

---
+++ src/pages/GameRoomPage/GameRoomSetting.tsx
@@ -0,0 +1,11 @@
+const GameRoomSetting = () => {
+  return (
+    <div className='w-[20%] flex justify-center items-center'>
+      <button className='w-[8rem] aspect-square text-white text-[2rem] bg-gray-300/90 hover:bg-gray-200 transition-all duration-300 shadow-md shadow-black/50 rounded-[2.5rem]'>
+        변경
+      </button>
+    </div>
+  );
+};
+
+export default GameRoomSetting;
