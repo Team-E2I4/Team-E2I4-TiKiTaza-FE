21be49e6c2238d6288f956d847cebd8c2bda82ad
dlsxjzld <sinjuk1@naver.com>
2024-02-18 14:07:15 +0900

    [🎨style]: 게임모드설명, 링크 초대, 시작 버튼 width height 조정

--- src/pages/GameRoomPage/GameRoomLinkInvitation.tsx
+++ src/pages/GameRoomPage/GameRoomLinkInvitation.tsx
@@ -1,6 +1,6 @@
 const GameRoomLinkInvitation = () => {
   return (
-    <button className='w-[28.1rem] h-[10rem] flex justify-center items-center text-[2rem] bg-coral-50 hover:bg-coral-100 transition-all duration-300 shadow-md shadow-black/50 rounded-[2.5rem]'>
+    <button className='w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] bg-coral-50 hover:bg-coral-100 transition-all duration-300 shadow-md shadow-black/50 rounded-[2.5rem]'>
       링크 초대
     </button>
   );
