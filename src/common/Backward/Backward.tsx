d378d7aaa0d113fa0774899835057a613dad0c3f
dlsxjzld <sinjuk1@naver.com>
2024-02-18 14:19:53 +0900

    [✨feat]: 뒤로가기 공용 컴포넌트 구현

---
+++ src/common/Backward/Backward.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import backward from '@/assets/backward.png';
+
+const Backward = () => {
+  // TODO: 예외처리 사항 필요하면 추후에 추가
+  const navigate = useNavigate();
+  const goPreviousPage = () => {
+    navigate(-1);
+  };
+
+  return (
+    <button
+      type='button'
+      onClick={goPreviousPage}>
+      <img
+        src={backward}
+        className='w-[4.8rem]'
+        alt='뒤로가기'
+      />
+    </button>
+  );
+};
+
+export default Backward;
