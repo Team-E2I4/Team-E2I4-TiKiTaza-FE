90403dce20af395e59cce651fc098ea4f795ffb5
dlsxjzld <sinjuk1@naver.com>
2024-02-18 14:07:15 +0900

    [🎨style]: 게임모드설명, 링크 초대, 시작 버튼 width height 조정

--- src/pages/GameRoomPage/GameModeInfo.tsx
+++ src/pages/GameRoomPage/GameModeInfo.tsx
@@ -6,8 +6,8 @@ interface GameModeInfoProps {
 
 const GameModeInfo = ({ children }: GameModeInfoProps) => {
   return (
-    <div className='w-[61.3rem] h-[15rem] flex bg-beige-100 shadow-md shadow-black/50 rounded-[2.5rem]'>
-      <p className='w-[80%] my-[2.5rem] ml-[2rem] overflow-y-scroll text-center'>
+    <div className='w-[69.3rem] h-[10rem] flex bg-beige-100 shadow-md shadow-black/50 rounded-[2.5rem]'>
+      <p className='w-[80%] my-[2.5rem] ml-[2rem] overflow-y-scroll text-left'>
         게임 모드 설명이 매우 길어 진다면 어떻게 될까요. 궁금해집니다. 한번
         알아볼까요? 한번 알아볼까요???
         <br /> 한번 알아볼까요
