b79c20e81c094757cfcc0732aa4e8ec9e960a057
dlsxjzld <sinjuk1@naver.com>
2024-02-20 14:54:26 +0900

    [🎨style]: 선택한 mode에 따라 색상 및 모양 변경

--- src/pages/MainPage/CreateRoomModal.tsx
+++ src/pages/MainPage/CreateRoomModal.tsx
@@ -10,6 +10,7 @@ interface CreateRoomModalProps {
 
 const CreateRoomModal = ({ children, className }: CreateRoomModalProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedMode, setSelectedMode] = useState('sentence');
 
   const inputList = [
     {
@@ -34,20 +35,28 @@ const CreateRoomModal = ({ children, className }: CreateRoomModalProps) => {
   const selectList = [
     {
       name: '인원수',
-      optionValues: Array.from({ length: 8 }).map((_, index) => ({
-        value: index + 1,
-        text: `${index + 1}명`,
-      })),
+      optionValues: Array.from({ length: 8 })
+        .map((_, index) => ({
+          value: index + 1,
+          text: `${index + 1}명`,
+        }))
+        .slice(1),
     },
     {
       name: '라운드',
-      optionValues: Array.from({ length: 8 }).map((_, index) => ({
-        value: index + 1,
-        text: `${index + 1}라운드`,
-      })),
+      optionValues: Array.from({ length: 8 })
+        .map((_, index) => ({
+          value: index + 1,
+          text: `${index + 1}라운드`,
+        }))
+        .slice(1),
     },
   ];
-  const modeList = ['문장 모드', '코딩 모드', '단어 모드'];
+  const modeList = [
+    { value: '문장 모드', mode: 'sentence' },
+    { value: '코딩 모드', mode: 'coding' },
+    { value: '단어 모드', mode: 'word' },
+  ];
 
   return (
     <Dialog.Root
@@ -61,12 +70,12 @@ const CreateRoomModal = ({ children, className }: CreateRoomModalProps) => {
           }
         />
         <Dialog.Content
-          className={`rounded-[1rem] border-green-100 border-[0.3rem] w-[90rem] h-[60rem] flex flex-col items-start justify-start p-[2rem] bg-white z-10 fixed inset-1/2 translate-x-[-50%] translate-y-[-50%] ${className}`}>
+          className={`rounded-[1rem] border-green-100 border-[0.3rem] w-[95rem] h-[60rem] flex flex-col items-start justify-start p-[2rem] bg-white z-10 fixed inset-1/2 translate-x-[-50%] translate-y-[-50%] ${className}`}>
           <Dialog.Title className='text-[2.4rem] font-semibold'>
             방 만들기
           </Dialog.Title>
-          <div className='flex flex-1 w-full'>
-            <Form.Root className='w-1/2 '>
+          <div className='flex flex-1 w-full text-[1.8rem]'>
+            <Form.Root className='flex flex-col w-1/2 gap-[3.5rem]'>
               {inputList.map(
                 ({
                   name,
@@ -80,10 +89,11 @@ const CreateRoomModal = ({ children, className }: CreateRoomModalProps) => {
                     className='flex justify-between'
                     key={name}
                     name={name}>
-                    <Form.Label>{labelText}</Form.Label>
-                    <div className='flex flex-col gap-[1rem]'>
+                    <Form.Label className=''>{labelText}</Form.Label>
+                    <div className='flex flex-col'>
                       <Form.Control asChild>
                         <input
+                          className='w-[22.5rem] h-[3.6rem] p-[1rem] border-[0.2rem] border-gray-200 rounded-[0.8rem]'
                           type={type}
                           placeholder={placeholder}
                           pattern={validatePattern}
@@ -99,31 +109,48 @@ const CreateRoomModal = ({ children, className }: CreateRoomModalProps) => {
                   </Form.Field>
                 )
               )}
-              {selectList.map(({ name, optionValues }) => (
-                <Form.Field
-                  key={name}
-                  name={name}>
-                  <Form.Label>{name}</Form.Label>
-                  <Form.Control asChild>
-                    <select>
-                      {optionValues.map(({ value, text }) => (
-                        <option
-                          key={text}
-                          value={value}>
-                          {text}
-                        </option>
-                      ))}
-                    </select>
-                  </Form.Control>
-                </Form.Field>
-              ))}
-              <article className='flex flex-col'>
-                {modeList.map((mode) => (
+              <div className='flex justify-between'>
+                {selectList.map(({ name, optionValues }) => (
+                  <Form.Field
+                    key={name}
+                    name={name}>
+                    <Form.Label>{name}</Form.Label>
+                    <Form.Control asChild>
+                      <select className='border-[0.2rem] border-gray-200 rounded-[0.8rem]'>
+                        {optionValues.map(({ value, text }) => (
+                          <option
+                            key={text}
+                            value={value}>
+                            {text}
+                          </option>
+                        ))}
+                      </select>
+                    </Form.Control>
+                  </Form.Field>
+                ))}
+              </div>
+              <article className='flex flex-col gap-[2rem]'>
+                {modeList.map(({ value, mode }) => (
                   <button
-                    type='button'
-                    key={mode}
-                    className='text-left'>
-                    {mode}
+                    onClick={() => {
+                      setSelectedMode(mode);
+                      // TODO : 버튼 클릭시 오른쪽 섹션에 모드의 인게임 사진 보여줘야함
+                    }}
+                    key={value}
+                    className={`relative inline-block h-[6rem] text-left transition-all rounded-tl-[0.8rem] 
+                      ${
+                        selectedMode !== mode
+                          ? `w-[23rem] bg-gray-100`
+                          : `w-[30rem] bg-coral-100`
+                      }`}>
+                    {value}
+                    <div
+                      className={`absolute bg-none border-r-[3rem] border-t-[6rem] border-solid border-r-transparent h-0 left-auto top-0 w-0 transition-all
+                        ${
+                          selectedMode !== mode
+                            ? `border-gray-100 right-0`
+                            : `border-coral-100 right-[-3rem]`
+                        }`}></div>
                   </button>
                 ))}
               </article>
