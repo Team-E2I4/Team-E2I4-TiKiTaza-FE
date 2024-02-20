94cd46eccfc6ba0ef2550edf56b2b2314957ef76
yejinleee <yejin9487@gmail.com>
2024-02-20 12:05:55 +0900

    [🎨style]: 자동차 이동 방향 타입 Enum 선언

--- src/common/Layout/BodyLayout.tsx
+++ src/common/Layout/BodyLayout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const BodyLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex-1 w-layout-w px-[2rem] pt-[4rem] flex flex-col relative'>
+    <div className='flex-1 w-layout-w px-[2rem] pt-[4rem] flex flex-col'>
       {children}
     </div>
   );
