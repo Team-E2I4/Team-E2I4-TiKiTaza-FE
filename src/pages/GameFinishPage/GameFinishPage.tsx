c01628326c67bdd75944009698479e0a8c2d3c47
Baek <woonn97@gmail.com>
2024-02-20 13:12:36 +0900

    [🎨style]: 랭킹 색상 수정

--- src/pages/GameFinishPage/GameFinishPage.tsx
+++ src/pages/GameFinishPage/GameFinishPage.tsx
@@ -11,16 +11,35 @@ const GameFinishPage = () => {
       </section>
       <section className='flex gap-[10rem] w-full'>
         <div className='flex flex-1 justify-center items-end'>
-          <div className='bg-beige-100 h-[28rem] w-[14rem] flex items-center justify-center shadow text-5xl font-bold'>
+          <div
+            className='h-[28rem] w-[14rem] flex items-center justify-center shadow-2xl text-5xl font-bold'
+            style={{
+              backgroundImage:
+                'linear-gradient(0deg, #8B8C8D 0%, #C0C0C0 25%, #6D6E71 50%, #F5F5F5 100%)',
+              boxShadow: '0 3px 10px rgba(0, 0, 0, 0.2)',
+            }}>
             2
           </div>
-          <div className='bg-beige-100 h-[40rem] w-[14rem] flex items-center justify-center shadow text-5xl font-bold'>
+          <div
+            className='h-[40rem] w-[14rem] flex items-center justify-center shadow-2xl text-5xl font-bold'
+            style={{
+              backgroundImage:
+                'linear-gradient(0deg, #ECC440 0%, #FFFA8A 25%, #DDAC17 50%, #FFFF95 100%)',
+              boxShadow: '0 3px 10px rgba(0, 0, 0, 0.2)',
+            }}>
             1
           </div>
-          <div className='bg-beige-100 h-[20rem] w-[14rem] flex items-center justify-center shadow text-5xl font-bold'>
+          <div
+            className='h-[20rem] w-[14rem] flex items-center justify-center shadow-2xl text-5xl font-bold'
+            style={{
+              backgroundImage:
+                'linear-gradient(0deg, #CD7F32 0%, #E89752 25%, #8B4513 50%, #CD7F32 100%)',
+              boxShadow: '0 3px 10px rgba(0, 0, 0, 0.2)',
+            }}>
             3
           </div>
         </div>
+
         <div className='flex-1'>
           <RankList data={DUMMY_DATA} />
         </div>
