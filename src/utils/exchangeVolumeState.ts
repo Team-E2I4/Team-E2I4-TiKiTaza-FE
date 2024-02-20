e3da59dad2e338f37c7eb06739c7d51a7ad26dcf
loevray <rei980213@gmail.com>
2024-02-20 11:10:45 +0900

    [🚚change]: 헤더 폴더구조 변경

--- src/utils/exchangeVolumeState.ts
+++ src/utils/exchangeVolumeState.ts
@@ -1,5 +1,5 @@
+import { PAUSE, PLAY } from '@/common/Header/constants/volume';
 import { VolumeType } from '@/common/Header/Header';
-import { PAUSE, PLAY } from '@/constants/volume';
 
 export const exchangeVolumeState = (currentState: VolumeType) => {
   return currentState === PLAY ? PAUSE : PLAY;
