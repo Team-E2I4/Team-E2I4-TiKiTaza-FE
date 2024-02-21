37d6636d3903a9ccbfba91b9b2da8ae15992b503
Baek <woonn97@gmail.com>
2024-02-21 10:29:27 +0900

    [✨feat]: react-query 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 
 export const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+      },
+    },
+  });
   return (
     <>
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 };
