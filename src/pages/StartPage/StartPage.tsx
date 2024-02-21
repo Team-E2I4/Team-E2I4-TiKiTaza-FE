ecc70366f0c4dd6b7635d8fc5106eb4f70eb09f8
yejinleee <yejin9487@gmail.com>
2024-02-21 11:47:37 +0900

    [✨feat]: 진입화면, 헤더로고에 navigate 연결

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom';
 import logo_big_shadow from '@/assets/logo_big_shadow.png';
 
 const StartPage = () => {
+  const navigate = useNavigate();
   return (
     <main className='w-full h-full flex flex-col justify-center items-center gap-[2rem]'>
       <img
@@ -8,7 +10,11 @@ const StartPage = () => {
         src={logo_big_shadow}
         className='object-cover h-[55rem]'
       />
-      <button className='w-[33.9rem] h-[8.8rem] font-[Giants-Inline] text-[3.2rem] shadow-md shadow-black/50 bg-green-100 hover:bg-green-200 transition-all rounded-full'>
+      <button
+        onClick={() => {
+          navigate('/main');
+        }}
+        className='w-[33.9rem] h-[8.8rem] font-[Giants-Inline] text-[3.2rem] shadow-md shadow-black/50 bg-green-100 hover:bg-green-200 transition-all rounded-full'>
         시작하기
       </button>
       <span className='font-bold font-[Giants-Inline] text-[3.2rem]'>
