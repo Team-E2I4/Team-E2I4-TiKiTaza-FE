1718f1c9b41eb25b3454807b2beae59a13e65d54
loevray <rei980213@gmail.com>
2024-02-22 23:13:18 +0900

    [🐛fix]: eslintignore추가하여 config파일들 lint안하게 변경

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
+/// <reference types="vitest" />
 export default defineConfig({
   plugins: [react()],
   resolve: {
