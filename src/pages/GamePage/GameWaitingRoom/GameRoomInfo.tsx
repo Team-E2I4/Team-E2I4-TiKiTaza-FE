835f357846116bf21cbe84899a12273169b093bf
yejinleee <yejin9487@gmail.com>
2024-02-24 02:36:30 +0900

    [🎨style]: 응답값으로 헤더 반영

--- src/pages/GamePage/GameWaitingRoom/GameRoomInfo.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameRoomInfo.tsx
@@ -1,4 +1,5 @@
 import { I_GameRoomResponse } from '@/ws/types/wsResType';
+import { GAME_TYPE } from '../constants';
 
 const GameRoomInfo = ({
   gameRoomInfo,
@@ -8,13 +9,14 @@ const GameRoomInfo = ({
   return (
     <div className='w-[105rem] h-[7.1rem] text-[2rem] flex bg-beige-100 rounded-[2.5rem] shadow-md shadow-black/50 items-center '>
       <div className='flex justify-center items-center w-[25%] h-[70%] border-r border-solid border-black'>
-        {gameRoomInfo.roomId}
+        No.{gameRoomInfo.roomId}
       </div>
       <div className='flex justify-center items-center w-[35%] h-[70%] border-r border-solid border-black'>
         {gameRoomInfo.roomInfo?.title}
       </div>
       <div className='flex justify-center items-center w-[20%] h-[70%] border-r border-solid border-black'>
-        {gameRoomInfo.roomInfo?.mode}
+        {gameRoomInfo.roomInfo &&
+          GAME_TYPE.get(gameRoomInfo.roomInfo?.gameMode)}
       </div>
       <div className='flex justify-center items-center w-[20%] h-[70%]'>{`${gameRoomInfo.allMembers?.length} / ${gameRoomInfo.roomInfo?.maxPlayer}`}</div>
     </div>
