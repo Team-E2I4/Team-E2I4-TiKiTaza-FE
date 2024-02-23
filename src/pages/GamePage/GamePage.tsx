c7f4c2139b09e2176be0fe48ece2bbb1ef1ed7af
yejinleee <yejin9487@gmail.com>
2024-02-24 02:14:02 +0900

    [✨feat]: 리턴문형식 Funnel로 수정

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,7 +1,6 @@
 import { Stomp } from '@stomp/stompjs';
 import { useEffect, useState } from 'react';
 import { I_GameRoomResponse } from '@/ws/types/wsResType';
-// import useWebsocket from '@/ws/useWebsocket';
 import GameCode from './GameCode/GameCode';
 import GameFinish from './GameFinish/GameFinish';
 import GameSentence from './GameSentence/GameSentence';
@@ -72,27 +71,27 @@ const GamePage = () => {
   // TODO:
   // 여기서 zustand 전역상태값(초대로 들어온 사람이라면 url의 해시값->정제->유효검사 후 상태값) 으로 방번호 추출
   // 방번호를 가지고 게임 상태에 대해 api 요청
-  // const { initWebsocket } = useWebsocket();
 
-  const [gameMode] = useState<GameModeType>('waiting'); //,,,
+  const gameMode: GameModeType = 'waiting';
 
-  switch (gameMode) {
-    case 'waiting':
-      return (
-        'roomId' in gameRoomInfo && (
+  return (
+    <>
+      {'roomId' in gameRoomInfo ? (
+        gameMode === 'waiting' ? (
           <GameWaitingRoom gameRoomInfo={gameRoomInfo} />
+        ) : gameMode === 'sentence' ? (
+          <GameSentence />
+        ) : gameMode === 'code' ? (
+          <GameCode />
+        ) : gameMode === 'word' ? (
+          <GameWord />
+        ) : (
+          <GameFinish />
         )
-      );
-    case 'sentence':
-      return <GameSentence />;
-    case 'code':
-      return <GameCode />;
-    case 'word':
-      return <GameWord />;
-    case 'finish':
-      return <GameFinish />;
-    default:
-      return <GameWaitingRoom />;
-  }
+      ) : (
+        <div>소켓오류</div>
+      )}
+    </>
+  );
 };
 export default GamePage;
