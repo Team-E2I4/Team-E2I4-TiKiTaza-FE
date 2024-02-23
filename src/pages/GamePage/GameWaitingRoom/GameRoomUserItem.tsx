a8e9c83fa45f83f099b061ab150940bb755f87c4
yejinleee <yejin9487@gmail.com>
2024-02-24 01:11:21 +0900

    [✨feat]: GamePage에서 소켓연결 및 응답 데이터로 대기실 유저 보여줌

--- src/pages/GamePage/GameWaitingRoom/GameRoomUserItem.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameRoomUserItem.tsx
@@ -1,13 +1,9 @@
 import * as Avatar from '@radix-ui/react-avatar';
 import close from '@/assets/close.png';
-import { I_GameRoomUserCard } from '../types/gameInfoType';
+import { I_AllMember } from '@/ws/types/wsResType';
 
-const GameRoomUserItem = ({
-  userName,
-  rank,
-  userImage,
-  userImageFallbackDelay,
-}: I_GameRoomUserCard) => {
+const GameRoomUserItem = ({ nickname }: I_AllMember) => {
+  const rank = 3; // memberId로 회원조회!!
   return (
     <div className='w-[25.8rem] h-[21.2rem] flex flex-col bg-white shadow-md shadow-black/50 rounded-[2.5rem]'>
       <button className='self-end pt-[1.6rem] pr-[1.6rem] '>
@@ -21,16 +17,17 @@ const GameRoomUserItem = ({
         <Avatar.Root className='w-1/2 self-center'>
           <Avatar.Image
             className='size-[10rem] rounded-full'
-            src={userImage}
+            src={
+              'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80'
+            }
             alt='프로필 이미지'
           />
-          <Avatar.Fallback delayMs={userImageFallbackDelay}>
-            test
-          </Avatar.Fallback>
+          {/*임시 이미지*/}
+          <Avatar.Fallback delayMs={6000}>test</Avatar.Fallback>
         </Avatar.Root>
         <div className='w-1/2 flex flex-col items-center justify-around text-center'>
-          <div className='w-[10rem] py-[0.4rem] bg-green-70 rounded-[2.5rem] '>
-            {userName}
+          <div className='w-[10rem] py-[0.4rem] bg-green-70 rounded-[2.5rem]'>
+            {nickname}
           </div>
           <div className='w-[10rem] py-[0.4rem] bg-green-70 rounded-[2.5rem]'>
             {rank}등
