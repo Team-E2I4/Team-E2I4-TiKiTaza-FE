732a55be6f5e368eeda37d19343314a371b3cd28
yejinleee <yejin9487@gmail.com>
2024-02-24 02:15:57 +0900

    [🐛fix]: 대기실 내부 컴포넌트 전달 프롭스 및 타입 수정

--- src/pages/GamePage/GameWaitingRoom/GameRoomUserList.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameRoomUserList.tsx
@@ -9,12 +9,13 @@ const GameRoomUserList = ({
   return (
     <>
       <main className='flex-1 grid grid-rows-2 grid-cols-4 gap-x-[4rem] gap-y-[3rem]'>
-        {gameRoomUserList.map((gameRoomUser) => (
-          <GameRoomUserItem
-            key={gameRoomUser.memberId}
-            {...gameRoomUser}
-          />
-        ))}
+        {gameRoomUserList &&
+          gameRoomUserList.map((gameRoomUser) => (
+            <GameRoomUserItem
+              key={gameRoomUser.memberId}
+              {...gameRoomUser}
+            />
+          ))}
       </main>
     </>
   );
