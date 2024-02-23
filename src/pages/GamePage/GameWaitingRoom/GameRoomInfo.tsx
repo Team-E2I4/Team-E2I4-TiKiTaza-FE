6e5fa0fca1584dda6e8a66e3a30dc7ab6dd7144e
yejinleee <yejin9487@gmail.com>
2024-02-24 02:15:57 +0900

    [🐛fix]: 대기실 내부 컴포넌트 전달 프롭스 및 타입 수정

--- src/pages/GamePage/GameWaitingRoom/GameRoomInfo.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameRoomInfo.tsx
@@ -1,22 +1,22 @@
-import { I_AllMember, I_RoomInfo } from '@/ws/types/wsResType';
+import { I_GameRoomResponse } from '@/ws/types/wsResType';
 
-interface GameRoomInfoProps {
-  roomInfo: I_RoomInfo;
-  allMembers: I_AllMember[];
-}
-const GameRoomInfo = ({ roomInfo, allMembers }: GameRoomInfoProps) => {
+const GameRoomInfo = ({
+  gameRoomInfo,
+}: {
+  gameRoomInfo: I_GameRoomResponse;
+}) => {
   return (
     <div className='w-[105rem] h-[7.1rem] text-[2rem] flex bg-beige-100 rounded-[2.5rem] shadow-md shadow-black/50 items-center '>
       <div className='flex justify-center items-center w-[25%] h-[70%] border-r border-solid border-black'>
-        {roomInfo.id}
+        {gameRoomInfo.roomId}
       </div>
       <div className='flex justify-center items-center w-[35%] h-[70%] border-r border-solid border-black'>
-        {roomInfo.title}
+        {gameRoomInfo.roomInfo?.title}
       </div>
       <div className='flex justify-center items-center w-[20%] h-[70%] border-r border-solid border-black'>
-        {roomInfo.mode}
+        {gameRoomInfo.roomInfo?.mode}
       </div>
-      <div className='flex justify-center items-center w-[20%] h-[70%]'>{`${allMembers.length} / ${roomInfo.maxPlayer}`}</div>
+      <div className='flex justify-center items-center w-[20%] h-[70%]'>{`${gameRoomInfo.allMembers?.length} / ${gameRoomInfo.roomInfo?.maxPlayer}`}</div>
     </div>
   );
 };
