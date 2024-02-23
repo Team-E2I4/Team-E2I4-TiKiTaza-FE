cf18dac2a282e7f8684358f709a0c76b5598b40c
yejinleee <yejin9487@gmail.com>
2024-02-24 01:11:21 +0900

    [✨feat]: GamePage에서 소켓연결 및 응답 데이터로 대기실 유저 보여줌

--- src/pages/GamePage/GameWaitingRoom/GameRoomInfo.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameRoomInfo.tsx
@@ -1,24 +1,22 @@
-import { I_GameInfo } from '../types/gameInfoType';
+import { I_AllMember, I_RoomInfo } from '@/ws/types/wsResType';
 
-const GameRoomInfo = ({
-  gameRoomId,
-  gameRoomName,
-  gameMode,
-  gameRoomMaximumHeadCount,
-  gameRoomUserList,
-}: I_GameInfo) => {
+interface GameRoomInfoProps {
+  roomInfo: I_RoomInfo;
+  allMembers: I_AllMember[];
+}
+const GameRoomInfo = ({ roomInfo, allMembers }: GameRoomInfoProps) => {
   return (
     <div className='w-[105rem] h-[7.1rem] text-[2rem] flex bg-beige-100 rounded-[2.5rem] shadow-md shadow-black/50 items-center '>
       <div className='flex justify-center items-center w-[25%] h-[70%] border-r border-solid border-black'>
-        {gameRoomId}
+        {roomInfo.id}
       </div>
       <div className='flex justify-center items-center w-[35%] h-[70%] border-r border-solid border-black'>
-        {gameRoomName}
+        {roomInfo.title}
       </div>
       <div className='flex justify-center items-center w-[20%] h-[70%] border-r border-solid border-black'>
-        {gameMode}
+        {roomInfo.mode}
       </div>
-      <div className='flex justify-center items-center w-[20%] h-[70%]'>{`${gameRoomUserList.length} / ${gameRoomMaximumHeadCount}`}</div>
+      <div className='flex justify-center items-center w-[20%] h-[70%]'>{`${allMembers.length} / ${roomInfo.maxPlayer}`}</div>
     </div>
   );
 };
