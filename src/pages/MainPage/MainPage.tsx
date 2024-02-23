97ecd125fe453c13a8a28c493f87066e88253c66
loevray <rei980213@gmail.com>
2024-02-23 13:24:43 +0900

    [✨feat]: useSSE훅 완료 및 EventSourceEventMap에 커스텀 타입 추가

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,24 +1,9 @@
-import { EventSourcePolyfill } from 'event-source-polyfill';
-import { useEffect } from 'react';
 import CreateRoomModal from './CreateRoom/CreateRoomModal';
 import GameRoomList from './GameRoomList';
 import UserCard from './UserCard';
 import UserList from './UserList';
 
 const MainPage = () => {
-  useEffect(() => {
-    const sse = new EventSourcePolyfill(
-      'http://ec2-3-38-182-155.ap-northeast-2.compute.amazonaws.com/api/v1/sse',
-      {
-        headers: {
-          Authorization: `Bearer ${stored}`,
-        },
-        withCredentials: true,
-      }
-    );
-    //서버측 emitter네임과 클라이언트측 이벤트 이름이 같아야 함
-    sse.addEventListener('changeGameRoom', () => {});
-  }, []);
   return (
     <main className='flex pb-[4rem] gap-[3rem]'>
       <section className='flex flex-col gap-[3rem] w-[25rem]'>
