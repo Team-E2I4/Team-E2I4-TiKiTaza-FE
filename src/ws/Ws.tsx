a80e3f87a2d02668105abffd94d2dfc764fc8e4a
yejinleee <yejin9487@gmail.com>
2024-02-23 11:39:50 +0900

    [✨feat]: publish received 수정
    Co-authored-by: dlsxjzld <sinjuk1@naver.com>

--- src/ws/Ws.tsx
+++ src/ws/Ws.tsx
@@ -1,6 +1,6 @@
 import { Stomp } from '@stomp/stompjs';
 import { useEffect } from 'react';
-import SockJS from 'sockjs-client'; // 주석처리필요
+import SockJS from 'sockjs-client';
 import { guestLogin } from '@/apis/api';
 const { VITE_API_WS_END_POINT } = import.meta.env;
 
@@ -11,6 +11,7 @@ const Ws = () => {
     localStorage.setItem('token', data.data?.accessToken);
     token = data.data.accessToken;
   };
+  const ROOMID_TEST = 3; // 테스트용 RoomId
   let token = '';
   const fromls = localStorage.getItem('token');
   if (!fromls) {
@@ -18,37 +19,43 @@ const Ws = () => {
   } else {
     token = fromls;
   }
+  const connectHeaders = {
+    Authorization: 'Bearer ' + token,
+  };
   useEffect(() => {
     stompClient.connect({ Authorization: `Bearer ${token}` }, () =>
       onConnected()
     );
   }, []);
-  const connectHeaders = {
-    Authorization: 'Bearer ' + token,
-    RoomId: `8`, // 없어도 되는
-  };
   const onConnected = () => {
     //TODO: roomId는 방입장 GET요청 응답값으로 사용
-    stompClient.subscribe(`/from/game-room/1`, () => {}, connectHeaders);
-    stompClient.subscribe(`/from/game-room/1/error`, () => {}, connectHeaders);
-    // console.log(`${8}번 방 연결 완료!`);
+    stompClient.subscribe(
+      `/from/game-room/${ROOMID_TEST}`,
+      (e) => onMessageReceived(e),
+      connectHeaders
+    );
+    stompClient.subscribe(
+      `/from/game-room/${ROOMID_TEST}/error`,
+      // eslint-disable-next-line no-console
+      (e) => console.log('----subscribe error----', e),
+      connectHeaders
+    );
   };
-  // const onMessageReceived = (e: any) => {
-  //   // called when the client receives a STOMP message from the server
-  //   console.log('???', e);
-  // };
+  const onMessageReceived = ({ body }: { body: string }) => {
+    const responsePublish = JSON.parse(body);
+    // called when the client receives a STOMP message from the server
+    // eslint-disable-next-line no-console
+    console.log('onMessageReceived---', responsePublish);
+  };
 
   const handleEnterGameRoom = (roomId: number) => {
     stompClient.send(
       `/to/game-room/${roomId}/enter`,
-      {
-        Authorization: `Bearer ${token}`,
-        RoomId: 8,
-      },
+      connectHeaders,
       '어어어어'
     );
   };
-  return <div onClick={() => handleEnterGameRoom(8)}>ws</div>;
+  return <div onClick={() => handleEnterGameRoom(ROOMID_TEST)}>ws</div>;
 };
 
 export default Ws;
