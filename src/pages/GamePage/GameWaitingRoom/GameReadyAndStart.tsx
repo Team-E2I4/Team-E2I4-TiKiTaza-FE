f71c6fde0c6737615401874812df476ab235d7e1
yejinleee <yejin9487@gmail.com>
2024-02-25 23:39:40 +0900

    [🎨style]: UserItem UI 수정 및 게임준비 반영

--- src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
@@ -1,6 +1,18 @@
-const GameReadyAndStart = () => {
+import { CompatClient } from '@stomp/stompjs';
+import storageFactory from '@/utils/storageFactory';
+
+const GameReadyAndStart = ({ ws }: { ws: CompatClient }) => {
+  const { getItem } = storageFactory(localStorage);
+
+  const token = getItem('token', '');
+  const connectHeaders = {
+    Authorization: 'Bearer ' + token,
+  };
   return (
     <button
+      onClick={() => {
+        ws.send(`/to/game-room/10/ready`, connectHeaders, '입장~');
+      }}
       className={`w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] bg-coral-50 hover:bg-coral-100 transition-all duration-300 shadow-md shadow-black/50 rounded-[2.5rem]`}>
       {/* TODO: 전부 준비 완료 되면 시작 버튼 색상 bg-coral-100으로 변경 */}
       시작
--- src/pages/GamePage/GameWaitingRoom/GameRoomUserItem.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameRoomUserItem.tsx
@@ -2,37 +2,41 @@ import * as Avatar from '@radix-ui/react-avatar';
 import close from '@/assets/close.png';
 import { I_AllMember } from '@/ws/types/wsResType';
 
-const GameRoomUserItem = ({ nickname }: I_AllMember) => {
+const GameRoomUserItem = ({ nickname, readyStatus }: I_AllMember) => {
   const rank = 3; // memberId로 회원조회!!
   return (
-    <div className='w-[25.8rem] h-[21.2rem] flex flex-col bg-white shadow-md shadow-black/50 rounded-[2.5rem]'>
-      <button className='self-end pt-[1.6rem] pr-[1.6rem] '>
+    <div className='w-[25.8rem] h-[21.2rem] p-[1.6rem] pb-[4rem] relative flex flex-col bg-white shadow-md shadow-black/50 rounded-[2.5rem]'>
+      <button className='self-end'>
         <img
           src={close}
           alt='강퇴'
           className='w-[3rem]'
         />
       </button>
-      <div className='px-[2rem] py-[1rem] flex gap-[2rem]'>
+      <div className='flex grow gap-[2rem]'>
         <Avatar.Root className='w-1/2 self-center'>
           <Avatar.Image
             className='size-[10rem] rounded-full'
             src={
-              'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80'
+              'https://images.unsplash.com/photo-1534278931827-8a259344abe7?q=80?&w=128&h=128&dpr=2&q=80'
             }
             alt='프로필 이미지'
           />
           {/*임시 이미지*/}
           <Avatar.Fallback delayMs={6000}>test</Avatar.Fallback>
         </Avatar.Root>
-        <div className='w-1/2 flex flex-col items-center justify-around text-center'>
-          <div className='w-[10rem] py-[0.4rem] bg-green-70 rounded-[2.5rem]'>
+        <div className='w-1/2 flex flex-col justify-evenly text-center'>
+          <div className='w-[10rem] py-[0.4rem] bg-green-70 rounded-[1rem]'>
             {nickname}
           </div>
-          <div className='w-[10rem] py-[0.4rem] bg-green-70 rounded-[2.5rem]'>
+          <div className='w-[5rem] py-[0.4rem] bg-green-70 rounded-[1rem] text-[1.4rem]'>
             {rank}등
           </div>
         </div>
+      </div>
+      <div
+        className={`absolute w-[13rem] h-[4rem] rounded-[2rem_0_2.5rem] right-0 bottom-0 text-center leading-[4rem] font-[Giants-Inline] ${readyStatus && 'bg-green-100'}`}>
+        {readyStatus && '준비'}
       </div>
     </div>
   );
