b930c73225ac41e064ededa1711c333559fca471
yejinleee <yejin9487@gmail.com>
2024-02-26 01:45:54 +0900

    [✨feat]: 이미시작된방, 게임중 탈주 시 main으로 라우팅

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,5 +1,6 @@
 import { CompatClient, Stomp } from '@stomp/stompjs';
 import { useEffect, useMemo, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { guestLogin } from '@/apis/api';
 import { checkEmptyObj } from '@/utils/checkEmptyObj';
 import storageFactory from '@/utils/storageFactory';
@@ -12,6 +13,8 @@ import WsError from './GameWaitingRoom/WsError';
 import GameWord from './GameWord/GameWord';
 
 const GamePage = () => {
+  const navigate = useNavigate();
+
   const [gameRoomInfo, setGameRoomInfo] = useState({} as I_GameRoomResponse);
   const ws = useRef<CompatClient | null>(null);
 
@@ -33,7 +36,7 @@ const GamePage = () => {
       Authorization: `Bearer ${token}`,
     };
 
-    const ROOMID_TEST = 35; // 테스트용 RoomId ////////////////////////////////////
+    const ROOMID_TEST = 36; // 테스트용 RoomId ////////////////////////////////////
 
     const onConnected = () => {
       //TODO: roomId는 방입장 GET요청 응답값으로 사용
@@ -70,17 +73,20 @@ const GamePage = () => {
   }, []);
 
   const isSuccess = useMemo(() => !checkEmptyObj(gameRoomInfo), [gameRoomInfo]);
-  const selectedMode = useMemo(
-    () => gameRoomInfo.roomInfo?.gameMode,
-    [gameRoomInfo.roomInfo?.gameMode]
+  const [selectedMode, isPlaying] = useMemo(
+    () => [gameRoomInfo.roomInfo?.gameMode, gameRoomInfo.roomInfo?.isPlaying],
+    [gameRoomInfo.roomInfo]
   );
   const didBossStart = useMemo(
     () => gameRoomInfo.type === 'START',
     [gameRoomInfo]
-  ); //모두 준비인상태에서 방장이 시작했다면 'START' type 이 옴
+  ); //모두 준비인상태에서 방장이 시작했다면 'START' type 이 옴 -> 참여자들 컴포넌트 전환 필요
 
   if (!isSuccess) {
     return <WsError />;
+  }
+  if (isPlaying) {
+    navigate('/main');
   }
   return (
     <>
