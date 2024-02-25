2c64d84e8fe544eb7d326f9b7b80e8f4b8bfd496
yejinleee <yejin9487@gmail.com>
2024-02-26 00:03:25 +0900

    [♻️refactor]: useRef null 타입 유니온 지정 *

--- src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
@@ -1,7 +1,7 @@
 import { CompatClient } from '@stomp/stompjs';
 import storageFactory from '@/utils/storageFactory';
 
-const GameReadyAndStart = ({ ws }: { ws: CompatClient }) => {
+const GameReadyAndStart = ({ ws }: { ws: CompatClient | null }) => {
   const { getItem } = storageFactory(localStorage);
 
   const token = getItem('token', '');
@@ -11,7 +11,7 @@ const GameReadyAndStart = ({ ws }: { ws: CompatClient }) => {
   return (
     <button
       onClick={() => {
-        ws.send(`/to/game-room/10/ready`, connectHeaders, '입장~');
+        ws && ws.send(`/to/game-room/10/ready`, connectHeaders, '입장~');
       }}
       className={`w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] bg-coral-50 hover:bg-coral-100 transition-all duration-300 shadow-md shadow-black/50 rounded-[2.5rem]`}>
       {/* TODO: 전부 준비 완료 되면 시작 버튼 색상 bg-coral-100으로 변경 */}
