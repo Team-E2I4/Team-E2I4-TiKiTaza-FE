06fd24528eee06b39ff79aff8963ace9cbedb4b6
loevray <rei980213@gmail.com>
2024-02-25 19:07:28 +0900

    [🐛fix]: SSE 에러타입 받는 데이터와 일치하게 수정 및 SSEFallBack에서도 출력해줌

--- src/pages/MainPage/SSEFallBack.tsx
+++ src/pages/MainPage/SSEFallBack.tsx
@@ -5,7 +5,9 @@ const SSEFallBack = ({ error }: { error?: SSEErrorType }) => {
     //원래 방 목록 스타일과 겹침. 중복되는 스타일 수정 필요
     <div className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 row-start-2 col-start-1 col-span-2'>
       방 목록이 없거나 에러 발생!
-      <span>{error?.status}</span>
+      <span>
+        에러 : {error?.status} {error?.error?.message}
+      </span>
     </div>
   );
 };
