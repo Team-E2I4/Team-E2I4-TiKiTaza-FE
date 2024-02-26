a2f7b10fe3fce264b36505f0670b3f61bf0e1f73
loevray <rei980213@gmail.com>
2024-02-26 09:25:05 +0900

    [🐛fix]: select도 hook-form으로 값 가져오기 위하여 type추가

--- src/pages/MainPage/CreateRoom/types/index.ts
+++ src/pages/MainPage/CreateRoom/types/index.ts
@@ -5,6 +5,11 @@ export interface I_CreateRoomInputName {
   roomPassword: string;
 }
 
+export interface I_CreateRoomSelectName {
+  roomRound: number;
+  roomMaxPlayer: number;
+}
+
 export interface I_CreateRoomInput {
   name: Path<I_CreateRoomInputName>;
   value: string;
@@ -13,4 +18,13 @@ export interface I_CreateRoomInput {
   label: string;
   placeholder: string;
   validate?: RegisterOptions;
+}
+
+export interface I_CreateRoomSelectOptionValues {
+  value: number;
+  text: string;
+}
+export interface I_CreateRoomSelect {
+  name: Path<I_CreateRoomSelectName>;
+  optionValues: I_CreateRoomSelectOptionValues[];
 }
