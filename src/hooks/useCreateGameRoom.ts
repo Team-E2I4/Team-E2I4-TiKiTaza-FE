fd3ee9ce1364d018ac564a11b3d5546261aa8677
loevray <rei980213@gmail.com>
2024-02-26 11:23:22 +0900

    [♻️refactor]: 에러 임시적으로 alert창으로 띄우기

--- src/hooks/useCreateGameRoom.ts
+++ src/hooks/useCreateGameRoom.ts
@@ -1,20 +1,30 @@
 import { useMutation } from '@tanstack/react-query';
+import { AxiosError, AxiosResponse } from 'axios';
 import { createGameRoom } from '@/apis/api';
+import {
+  ApiResponseGameRoomCreateResponse,
+  GameRoomCreateRequest,
+} from '@/generated';
 
 interface I_UseCreateGameRoomProps {
   onSuccess?: () => void;
-  onError?: () => void;
+  onError?: (e: Error | AxiosError) => void;
 }
 
 const useCreateGameRoom = ({
   onSuccess,
   onError,
 }: I_UseCreateGameRoomProps) => {
-  return useMutation({
+  return useMutation<
+    AxiosResponse<ApiResponseGameRoomCreateResponse>,
+    Error | AxiosError,
+    GameRoomCreateRequest,
+    unknown
+  >({
     mutationFn: createGameRoom,
     mutationKey: ['createGameRoom'],
     onSuccess,
-    onError,
+    onError: (e) => onError?.(e),
   });
 };
 
