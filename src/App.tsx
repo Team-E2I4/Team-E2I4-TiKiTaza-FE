eafe939333199cf530b9fb7e8f192e21d158db5f
Baek <woonn97@gmail.com>
2024-02-26 11:58:55 +0900

    [✨feat]: token Headers에 저장

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 
 export const App = () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: 1,
-      },
-    },
-  });
+  const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient}>
--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,4 +1,17 @@
+import axios from 'axios';
 import { DefaultApiFactory } from '@/generated';
+import { BASE_PATH } from '@/generated/base';
+import storageFactory from '@/utils/storageFactory';
+
+const { getItem } = storageFactory(localStorage);
+
+//임시로 MyToken이라는 값을 정해두었습니다
+const axiosInstance = axios.create({
+  headers: {
+    Authorization: `Bearer ${getItem('MyToken', '')}`,
+  },
+});
+
 export const {
   createGameRoom,
   deleteMemberAccount,
@@ -10,4 +23,4 @@ export const {
   reIssueAccessToken,
   signUp,
   updateMemberNickname,
-} = DefaultApiFactory();
+} = DefaultApiFactory(undefined, BASE_PATH, axiosInstance);
