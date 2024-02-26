ce9a51ff4ecb3ea09eaa87c6052181339f8693c5
dlsxjzld <sinjuk1@naver.com>
2024-02-26 11:30:42 +0900

    [✨feat]: stompjs 버전 업그레이드 및 custom hook으로 분리
    [♻️refactor]: useWebsocket type 수정
    
    [✨feat]: disconnect 될 때 소켓 연결 끊기
    
    [🗑️remove]: 안쓰는 console.log 제거
    
    [✨feat]: 방장일 경우 방 설정 변경 버튼 활성화
    
    [🎨style]: 방장이면 시작, 일반 유저면 준비 버튼 보이기
    
    [♻️refactor]: useWebsocket 수정(사이드 이펙트 최소화)
    
    [✨feat]: 일반 유저가 ready 할 수 있는 기능 추가

--- src/pages/GamePage/GameWaitingRoom/GameWaitingRoom.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameWaitingRoom.tsx
@@ -1,9 +1,11 @@
-import { CompatClient } from '@stomp/stompjs';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Backward from '@/common/Backward/Backward';
-import { I_GameRoomResponse } from '@/ws/types/wsResType';
 import DisconnectModal from '../DisconnectModal';
+import {
+  HandleReadyGameType,
+  I_GameRoomResponse,
+} from '../types/websocketType';
 import GameModeInfo from './GameModeInfo';
 import GameReadyAndStart from './GameReadyAndStart';
 import GameRoomInfo from './GameRoomInfo';
@@ -13,17 +15,25 @@ import GameRoomUserList from './GameRoomUserList';
 
 const GameWaitingRoom = ({
   gameRoomRes,
-  ws,
+  handleReadyGame,
 }: {
   gameRoomRes: I_GameRoomResponse;
-  ws: CompatClient;
+  handleReadyGame: HandleReadyGameType;
 }) => {
+  // TODO :
+  // main -> game 으로 enterGameRoom을 통해 넘어올 때
+  // navigate 에 state로 memberId를 넣어주거나,
+  // 본인 정보 조회 API를 통해 자기 자신의 id를 roomInfo의 hostId와 비교
+
   const navigate = useNavigate();
-
   const { allMembers, roomInfo } = gameRoomRes;
-  // 각각 optional 이라 undefined 일수 있다고 추론됨 ..
-
   const [isAlert, setIsAlert] = useState(false);
+
+  // 임시 TEST
+  const myUserId = 2;
+  const myRoomId = 14;
+  const isAdmin = myUserId === roomInfo?.hostId;
+
   const handleClickBackward = () => {
     setIsAlert(true);
   };
@@ -52,11 +62,14 @@ const GameWaitingRoom = ({
           />
         )}
         <footer className='w-[114.8rem] flex gap-[5rem]'>
-          <GameModeInfo>
-            <GameRoomSetting />
-          </GameModeInfo>
+          <GameModeInfo>{isAdmin && <GameRoomSetting />}</GameModeInfo>
           <GameRoomLinkInvitation />
-          <GameReadyAndStart ws={ws} />
+          <GameReadyAndStart
+            isAdmin={isAdmin}
+            allMembers={allMembers}
+            handleReadyGame={handleReadyGame}
+            myRoomId={myRoomId}
+          />
         </footer>
       </div>
     </>
