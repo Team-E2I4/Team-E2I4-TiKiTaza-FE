9df5ceca008620043b1e68d44150ad4358ce3eba
Baek <woonn97@gmail.com>
2024-02-26 11:58:55 +0900

    [✨feat]: token Headers에 저장

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,30 +1,23 @@
 import { useMutation } from '@tanstack/react-query';
 import { guestLoginFn } from '@/pages/StartPage/guestLoginFn';
+import storageFactory from '@/utils/storageFactory';
 
 export interface AuthProps {
   onSuccess?: () => void;
-  onError?: (error: unknown) => void;
 }
 
-export const useGuestLogin = ({ onSuccess, onError }: AuthProps = {}) => {
+const { setItem } = storageFactory(localStorage);
+
+export const useGuestLogin = ({ onSuccess }: AuthProps = {}) => {
   return useMutation({
     mutationFn: guestLoginFn,
     onSuccess: (response) => {
       if (response?.data?.accessToken) {
-        // 방어코드
-        localStorage.setItem('accessToken', response.data.accessToken);
+        setItem('MyToken', response.data.accessToken);
       }
-      // if (response?.data?.refreshToken) {
-      //   // 방어코드
-      //   localStorage.setItem('refreshToken', response.data.refreshToken);
-      // }
 
       if (onSuccess) {
         onSuccess();
-      }
-
-      if (onError) {
-        onError(response);
       }
     },
   });
