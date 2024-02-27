eb95934cd17c57826620785834f83986ab2b1cdb
yejinleee <yejin9487@gmail.com>
2024-02-27 11:24:06 +0900

    [✨feat]: 게임화면에서 메인으로 이동시 navigate(0) - 리로드를 통해 연결된 웹소켓을 끊습니다.

--- src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
@@ -1,17 +1,18 @@
 import { CompatClient } from '@stomp/stompjs';
 import storageFactory from '@/utils/storageFactory';
 
-const GameReadyAndStart = ({ ws }: { ws: CompatClient | null }) => {
+const GameReadyAndStart = ({ ws }: { ws: CompatClient }) => {
   const { getItem } = storageFactory(localStorage);
 
   const token = getItem('token', '');
   const connectHeaders = {
     Authorization: 'Bearer ' + token,
   };
+  //TODO: 현재 방번호 zustand store에서
   return (
     <button
       onClick={() => {
-        ws && ws.send(`/to/game-room/10/ready`, connectHeaders, '입장~');
+        ws && ws.send(`/to/game-room/36/ready`, connectHeaders, '입장~');
       }}
       className={`w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] bg-coral-50 hover:bg-coral-100 transition-all duration-300 shadow-md shadow-black/50 rounded-[2.5rem]`}>
       {/* TODO: 전부 준비 완료 되면 시작 버튼 색상 bg-coral-100으로 변경 */}
