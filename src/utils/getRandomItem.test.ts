c5cf69a82ac3fbc05b5123e7286941fc3b91fe16
loevray <rei980213@gmail.com>
2024-02-27 13:05:20 +0900

    [🚚change]: getRandomTitle => getRandomItem으로 도메인 제거 및 유틸폴더로 이동

--- src/utils/getRandomItem.test.ts
+++ src/utils/getRandomItem.test.ts
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, expect, test, vitest } from 'vitest';
-import getRandomTitle from './getRandomTitle';
+import getRandomItem from './getRandomItem';
 
 beforeEach(() => {
   vitest.spyOn(Math, 'random').mockReturnValue(0.9);
@@ -11,8 +11,8 @@ afterEach(() => {
 
 test('get random title in array', () => {
   expect(
-    getRandomTitle({
-      titles: ['티키타자 고다고', '너만 오면 고', '타자승부 한판 ㄱㄱ!!'],
+    getRandomItem({
+      items: ['티키타자 고다고', '너만 오면 고', '타자승부 한판 ㄱㄱ!!'],
     })
   ).toBe('타자승부 한판 ㄱㄱ!!');
 });
