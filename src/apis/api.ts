8e24c53f196922b37813e229dce43edcc68ed62e
loevray <rei980213@gmail.com>
2024-02-27 17:10:06 +0900

    [🐛fix]: api인스턴스 생성이 아닌 request할때마다 getItem으로 토큰 가져오게 수정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -6,10 +6,10 @@ import storageFactory from '@/utils/storageFactory';
 const { getItem } = storageFactory(localStorage);
 
 //임시로 MyToken이라는 값을 정해두었습니다
-const axiosInstance = axios.create({
-  headers: {
-    Authorization: `Bearer ${getItem('MyToken', '')}`,
-  },
+const axiosInstance = axios.create();
+axiosInstance.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${getItem('MyToken', '')}`;
+  return config;
 });
 
 export const {
