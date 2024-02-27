8885db12a65e7d485c10d340342767562bb7489a
yejinleee <yejin9487@gmail.com>
2024-02-27 23:19:33 +0900

    [🗑️remove]: 주석 제거 및 타입 수정

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -10,7 +10,7 @@ import WsError from './GameWaitingRoom/WsError';
 import GameWord from './GameWord/GameWord';
 
 const GamePage = () => {
-  // TODO: zustand 전역상태값(초대로 들어온 사람이라면 url의 해시값->정제->유효검사 후 상태값) 으로 방번호 추출
+  // TODO: 초대로 들어온 사람이라면 url의 해시값->정제->유효검사 후 상태값) 으로 방번호 추출
   const { roomId } = useRoomIdStore();
 
   const { gameRoomRes, handleReadyGame } = useWebsocket(roomId);
@@ -24,24 +24,20 @@ const GamePage = () => {
     () => [gameRoomRes.roomInfo?.gameMode, gameRoomRes.roomInfo?.isPlaying],
     [gameRoomRes.roomInfo]
   );
-  const didBossStart = useMemo(
+  const didAdminStart = useMemo(
     () => gameRoomRes.type === 'START',
     [gameRoomRes.type]
   ); //모두 준비인상태에서 방장이 시작했다면 'START' type 이 옴 -> 참여자들 컴포넌트 전환 필요
 
-  if (!isConnectSuccess) {
+  if (!roomId || !isConnectSuccess) {
     return <WsError />;
-  }
-  if (!roomId) {
-    // TODO: 진입으로 보내거나 어떤..
-    return <div>잘못된 접근</div>;
   }
   if (isPlaying) {
     navigate('/main');
   }
   return (
     <>
-      {didBossStart ? (
+      {didAdminStart ? (
         selectedMode === 'SENTENCE' ? (
           <GameSentence />
         ) : selectedMode === 'CODE' ? (
