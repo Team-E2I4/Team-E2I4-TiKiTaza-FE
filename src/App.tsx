efaea5d96f21657adcc0c9cb4f579abe38243589
loevray <rei980213@gmail.com>
2024-02-27 22:33:55 +0900

    [✨feat]: useAuth추가, HOC처럼 활용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet } from 'react-router-dom';
+import AuthRoute from './common/AuthRoute/AuthRoute';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,7 +16,11 @@ const queryClient = new QueryClient({
 export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Outlet />
+      <ErrorBoundary fallback={<div>연유를 모르는 에러</div>}>
+        <AuthRoute>
+          <Outlet />
+        </AuthRoute>
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 };
