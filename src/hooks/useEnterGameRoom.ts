21ec069110d032b2c0bccb836134865e0669b7c6
loevray <rei980213@gmail.com>
2024-02-27 15:51:37 +0900

    [🐛fix]: mutation에 전달할 params 타입 추가하여 에러 해결

--- src/hooks/useEnterGameRoom.ts
+++ src/hooks/useEnterGameRoom.ts
@@ -1,13 +1,25 @@
 import { useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 import { enterGameRoom } from '@/apis/api';
+import { ApiResponseGameRoomEnterResponse } from '@/generated';
 
 interface UseEnterGameRoomProps {
   onSuccess?: () => void;
 }
 
+export interface I_UseEnterGameRoomMutation {
+  roomId: number;
+  password?: string;
+}
+
 const useEnterGameRoom = ({ onSuccess }: UseEnterGameRoomProps) => {
-  return useMutation({
-    mutationFn: enterGameRoom,
+  return useMutation<
+    AxiosResponse<ApiResponseGameRoomEnterResponse>,
+    Error,
+    I_UseEnterGameRoomMutation
+  >({
+    mutationFn: ({ roomId, password }: I_UseEnterGameRoomMutation) =>
+      enterGameRoom(roomId, { password }),
     mutationKey: ['enterGameRoom'],
     onSuccess: () => {
       onSuccess?.();
