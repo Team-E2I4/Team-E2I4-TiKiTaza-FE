8ca52aa15727443984f8f9d4bea41d90b9d3ca2e
loevray <rei980213@gmail.com>
2024-02-27 22:33:55 +0900

    [✨feat]: useAuth추가, HOC처럼 활용

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,7 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { AxiosError, AxiosPromise } from 'axios';
+import { getMyProfileInfo } from '@/apis/api';
+import { ApiResponseAccountGetResponse, ErrorResponse } from '@/generated';
 import { guestLoginFn } from '@/pages/StartPage/guestLoginFn';
 import storageFactory from '@/utils/storageFactory';
 
@@ -17,6 +20,36 @@ export const useGuestLogin = ({ onSuccess }: AuthProps = {}) => {
       }
 
       onSuccess?.();
+    },
+  });
+};
+
+/*
+페이지 이동할때마다 새로 쿼리를 업데이트함. => 이동할때마다 인증처리 + 본인정보 저장
+캐시타임을 짧게 가져간다
+*/
+export const useAuthCheck = () => {
+  return useQuery<
+    AxiosPromise<ApiResponseAccountGetResponse>,
+    Error | AxiosError<ErrorResponse>
+  >({
+    queryFn: getMyProfileInfo,
+    queryKey: ['getMyProfileInfo'],
+    retryOnMount: false,
+    gcTime: 0,
+    throwOnError: (e) => {
+      if (!(e instanceof AxiosError)) {
+        return true;
+      }
+      if (
+        e.response?.status === 400 ||
+        e.response?.status === 401 ||
+        e.response?.status === 404 ||
+        e.response?.status === 409
+      ) {
+        return false;
+      }
+      return true;
     },
   });
 };
