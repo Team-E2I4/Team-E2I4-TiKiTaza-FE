490340e10040e288d65aa43e4bc2de2edb5d9ab7
dlsxjzld <sinjuk1@naver.com>
2024-02-28 13:24:48 +0900

    [✨feat]: gameStart 기능 추가

--- src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
@@ -1,9 +1,14 @@
-import { HandleReadyGameType, I_AllMember } from '../types/websocketType';
+import {
+  HandleReadyGameType,
+  HandleStartGameType,
+  I_AllMember,
+} from '../types/websocketType';
 
 interface GameReadyAndStartProps {
   isAdmin: boolean;
   allMembers: I_AllMember[] | undefined;
   handleReadyGame: HandleReadyGameType;
+  handleStartGame: HandleStartGameType;
   myRoomId: number;
 }
 
@@ -11,7 +16,8 @@ const GameReadyAndStart = ({
   isAdmin,
   allMembers,
   handleReadyGame,
-  myRoomId,
+  handleStartGame,
+  // myRoomId,
 }: GameReadyAndStartProps) => {
   //TODO : isAdmin에 따라 시작, 준비 버튼
   //TODO : allMembers의 readyStatus에 따라 방장이면 시작 버튼 활성화
@@ -24,6 +30,9 @@ const GameReadyAndStart = ({
   if (isAdmin) {
     return (
       <button
+        onClick={() => {
+          handleStartGame();
+        }}
         className={`w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] transition-all duration-300 shadow-md shadow-black/50 rounded-[2.5rem]
         ${isAllUserReady ? `bg-coral-100` : `bg-coral-50 opacity-[0.5] cursor-not-allowed`}
         `}>
@@ -35,7 +44,7 @@ const GameReadyAndStart = ({
   return (
     <button
       onClick={() => {
-        handleReadyGame(myRoomId);
+        handleReadyGame();
       }}
       className={`w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] bg-coral-50 hover:bg-coral-100 transition-all duration-300 shadow-md shadow-black/50 rounded-[2.5rem]`}>
       {/* TODO: 전부 준비 완료 되면 시작 버튼 색상 bg-coral-100으로 변경 */}
