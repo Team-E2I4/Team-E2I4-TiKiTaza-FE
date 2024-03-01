bf9f9972607e7559c0b61a16af6b0ae1d4e7984c
yejinleee <yejin9487@gmail.com>
2024-03-01 15:07:25 +0900

    [🎨style]: 폰트 사이즈 xl단위 -> rem 변경

--- src/pages/GamePage/GameFinish/GameFinish.tsx
+++ src/pages/GamePage/GameFinish/GameFinish.tsx
@@ -5,14 +5,14 @@ const GameFinish = () => {
   return (
     <div className='flex flex-col gap-[8rem] items-center'>
       <section>
-        <span className='text-3xl font-bold font-[Giants-Inline]'>
+        <span className='text-[3rem] font-bold font-[Giants-Inline]'>
           3초 뒤 자동으로 로비로 이동합니다...
         </span>
       </section>
       <section className='flex gap-[10rem] w-full'>
         <div className='flex flex-1 justify-center items-end'>
           <div
-            className='h-[28rem] w-[14rem] flex items-center justify-center shadow-2xl text-5xl font-bold'
+            className='h-[28rem] w-[14rem] flex items-center justify-center shadow-2xl text-[5rem] font-bold'
             style={{
               backgroundImage:
                 'linear-gradient(0deg, #8B8C8D 0%, #C0C0C0 25%, #6D6E71 50%, #F5F5F5 100%)',
@@ -21,7 +21,7 @@ const GameFinish = () => {
             2
           </div>
           <div
-            className='h-[40rem] w-[14rem] flex items-center justify-center shadow-2xl text-5xl font-bold'
+            className='h-[40rem] w-[14rem] flex items-center justify-center shadow-2xl text-[5rem] font-bold'
             style={{
               backgroundImage:
                 'linear-gradient(0deg, #ECC440 0%, #FFFA8A 25%, #DDAC17 50%, #FFFF95 100%)',
@@ -30,7 +30,7 @@ const GameFinish = () => {
             1
           </div>
           <div
-            className='h-[20rem] w-[14rem] flex items-center justify-center shadow-2xl text-5xl font-bold'
+            className='h-[20rem] w-[14rem] flex items-center justify-center shadow-2xl text-[5rem] font-bold'
             style={{
               backgroundImage:
                 'linear-gradient(0deg, #CD7F32 0%, #E89752 25%, #8B4513 50%, #CD7F32 100%)',
@@ -45,10 +45,10 @@ const GameFinish = () => {
         </div>
       </section>
       <section className='flex gap-10 font-[Giants-Inline]'>
-        <button className='bg-coral-100 px-6 py-6 rounded-[1rem] text-3xl font-bold'>
+        <button className='bg-coral-100 px-6 py-6 rounded-[1rem] text-[3rem] font-bold'>
           로비로 나가기
         </button>
-        <button className='bg-coral-100 px-6 py-6 rounded-[1rem] text-3xl font-bold'>
+        <button className='bg-coral-100 px-6 py-6 rounded-[1rem] text-[3rem] font-bold'>
           다시하기
         </button>
       </section>
