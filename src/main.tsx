7300ac07e8c2ff694c35b494e47a395495900922
loevray <rei980213@gmail.com>
2024-03-01 15:16:07 +0900

    [🗑️remove]: React strictmode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -68,7 +68,5 @@ export const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
   <RouterProvider router={router} />
-  // </React.StrictMode>
 );
