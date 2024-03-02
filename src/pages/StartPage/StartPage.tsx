f7e198e4a308035f7e9068fa32f4911f5c5d5e8f
Baek <woonn97@gmail.com>
2024-03-02 16:14:56 +0900

    [✨feat]: 코드 리뷰 반영

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,4 +1,3 @@
-import { ErrorBoundary } from 'react-error-boundary';
 import { useNavigate } from 'react-router-dom';
 import logo_big_shadow from '@/assets/logo_big_shadow.png';
 import { useGuestLogin } from '@/hooks/useAuth';
@@ -18,23 +17,21 @@ const StartPage = () => {
   };
 
   return (
-    <ErrorBoundary fallback={<div>게스트 로그인 에러</div>}>
-      <main className='w-full h-full flex flex-col justify-center items-center gap-[2rem]'>
-        <img
-          alt='티키타자 로고'
-          src={logo_big_shadow}
-          className='object-cover h-[55rem]'
-        />
-        <button
-          onClick={handleClick}
-          className='w-[33.9rem] h-[8.8rem] font-[Giants-Inline] text-[3.2rem] shadow-md shadow-black/50 bg-green-100 hover:bg-green-200 transition-all rounded-full'>
-          시작하기
-        </button>
-        <span className='font-bold font-[Giants-Inline] text-[3.2rem]'>
-          당신의 손가락이 빠르고 정확한지 확인해보세요!
-        </span>
-      </main>
-    </ErrorBoundary>
+    <main className='w-full h-full flex flex-col justify-center items-center gap-[2rem]'>
+      <img
+        alt='티키타자 로고'
+        src={logo_big_shadow}
+        className='object-cover h-[55rem]'
+      />
+      <button
+        onClick={handleClick}
+        className='w-[33.9rem] h-[8.8rem] font-[Giants-Inline] text-[3.2rem] shadow-md shadow-black/50 bg-green-100 hover:bg-green-200 transition-all rounded-full'>
+        시작하기
+      </button>
+      <span className='font-bold font-[Giants-Inline] text-[3.2rem]'>
+        당신의 손가락이 빠르고 정확한지 확인해보세요!
+      </span>
+    </main>
   );
 };
 
