3a4f54393c52bc20264b186739de441535ce5d36
yejinleee <yejin9487@gmail.com>
2024-03-05 01:09:02 +0900

    [💡comment] : 주석 삭제

--- src/store/useWordsStore.ts
+++ src/store/useWordsStore.ts
@@ -12,17 +12,5 @@ const useWordsStore = create<I_UseWordsStore>((set) => ({
   setWordUsed: (word: object) =>
     set((state) => ({ wordsStore: { ...state.wordsStore, ...word } })),
 }));
-// interface I_UseWordsStore {
-//   wordsStore: Map<string, number>;
-//   setWords: (wordsAll: Map<string, number>) => void;
-//   setWordUsed: (word: object) => void;
-// }
-
-// const useWordsStore = create<I_UseWordsStore>((set) => ({
-//   wordsStore: new Map(),
-//   setWords: (wordsAll: Map<string, number>) => set({ wordsStore: wordsAll }),
-//   setWordUsed: (word: object) =>
-//     set((state) => ({ wordsStore: { ...state.wordsStore, ...word } })),
-// }));
 
 export default useWordsStore;
