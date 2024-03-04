8b5f303e16ae4a112090567aba79976fb2fb2276
dlsxjzld <sinjuk1@naver.com>
2024-03-04 10:38:49 +0900

    [✨feat]: 인게임 헤더에 더미데이터 대신 useRoomInfoStore에 저장한 roomInfo 값을 추가

--- src/common/Ingame/IngameHeader.tsx
+++ src/common/Ingame/IngameHeader.tsx
@@ -1,19 +1,39 @@
-import { gameInfoDummy } from '@/pages/GamePage/GameDummys';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DisconnectModal from '@/pages/GamePage/common/DisconnectModal';
+import useRoomInfoStore from '@/store/useRoomInfoStore';
 import Backward from '../Backward/Backward';
 
 const IngameHeader = () => {
-  const { gameRoomName, gameRoomUserList, gameRoundTotal } = gameInfoDummy; // TODO : 캐싱쿼리값
-  const gameRoundCurrent = 2; //TODO: 현재 라운드로 수정. zustand?
+  const { roomInfo } = useRoomInfoStore();
+  const [isAlert, setIsAlert] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClickBackward = () => {
+    setIsAlert(true);
+  };
 
   return (
-    <div className='flex flex-row items-center gap-20 pb-12 font-[Giants-Inline]'>
-      <Backward />
-      <div className='w-[40rem] truncate text-[4rem]'>{gameRoomName}</div>
-      <div className='grow'>참여 {gameRoomUserList.length}명</div>
-      <div className='text-[3rem]'>
-        🏁 {gameRoundCurrent} / {gameRoundTotal}
+    <>
+      <DisconnectModal
+        isOpen={isAlert}
+        handleClickAction={() => {
+          navigate('/main', { replace: true });
+          navigate(0);
+        }}
+        handleClickCancel={() => {
+          setIsAlert(false);
+        }}
+      />
+      <div className='flex flex-row items-center gap-20 pb-12 font-[Giants-Inline] select-none'>
+        <Backward handleClickBackward={handleClickBackward} />
+        <div className='w-[40rem] truncate text-[4rem]'>{roomInfo?.title}</div>
+        <div className='grow'>참여 {roomInfo?.currentPlayer}명</div>
+        <div className='text-[3rem]'>
+          🏁 {roomInfo?.currentPlayer} / {roomInfo?.maxPlayer}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default IngameHeader;
