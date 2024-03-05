f8d4238bb05c244c5f8802a212c89f531f91f227
Baek <woonn97@gmail.com>
2024-03-05 11:34:14 +0900

    [✨feat]: Header 컴포넌트 수정 - 로그인 안했을 때, 로그인 버튼 추가

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -11,6 +11,7 @@ import logo_car from '@/assets/logo_car.png';
 import logo_taza from '@/assets/logo_taza.png';
 import { PAUSE, PLAY } from '@/common/Header/constants/volume';
 import { exchangeVolumeState } from '@/common/Header/utils/exchangeVolumeState';
+import { useAuthCheck } from '@/hooks/useAuth';
 import WrappedIcon from '../WrappedIcon/WrappedIcon';
 
 export type VolumeType = 'play' | 'pause';
@@ -36,6 +37,8 @@ const Header = () => {
     effect: PAUSE,
   });
   const navigate = useNavigate();
+
+  const { data } = useAuthCheck();
 
   return (
     <header className='bg-green-100 h-[4.5rem] w-[100%] shrink-0 flex justify-between px-[4rem]'>
@@ -71,14 +74,25 @@ const Header = () => {
           className='hover:bg-gray-100 size-[2rem] flex items-center justify-center'>
           <WrappedIcon IconComponent={mappedIcons.effect[volume.effect]} />
         </button>
-        <Avatar.Root className='cursor-pointer'>
-          <Avatar.Image
-            className='size-[3.5rem] rounded-full'
-            src='https://picsum.photos/id/237/200/300'
-            alt='프로필 이미지'
-          />
-          <Avatar.Fallback delayMs={1000}>프로필</Avatar.Fallback>
-        </Avatar.Root>
+        {data ? (
+          <Avatar.Root className='cursor-pointer'>
+            <Avatar.Image
+              className='size-[3.5rem] rounded-full'
+              src='https://picsum.photos/id/237/200/300'
+              alt='프로필 이미지'
+            />
+            <Avatar.Fallback delayMs={1000}>프로필</Avatar.Fallback>
+          </Avatar.Root>
+        ) : (
+          <button
+            onClick={() => {
+              navigate('/login');
+              navigate(0);
+            }}
+            className='hover:bg-gray-100 flex items-center justify-center'>
+            로그인
+          </button>
+        )}
       </section>
     </header>
   );
