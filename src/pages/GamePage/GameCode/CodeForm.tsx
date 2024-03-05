fb7cbfe4120b11c82fecedd87c02d7ef75edfed5
dlsxjzld <sinjuk1@naver.com>
2024-03-05 20:41:29 +0900

    [✨feat]: form 추가 및 모든 코드 다 제출시 input 태그 비활성화

--- src/pages/GamePage/GameCode/CodeForm.tsx
+++ src/pages/GamePage/GameCode/CodeForm.tsx
@@ -1,15 +1,20 @@
-import { ChangeEvent, KeyboardEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 interface CodeFormProps {
-  convertedDummyCode?: string[];
+  convertedDummyCode: string[];
 }
+
 const CodeForm = ({ convertedDummyCode }: CodeFormProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentInputValue, setCurrentInputValue] = useState('');
-
+  // const convertedDummyCode = ['ho', 'hi'];
   // console.log('convertedDummyCode', convertedDummyCode);
-  if (convertedDummyCode === undefined) {
-    return;
+  const isRoundFinish = currentIndex === convertedDummyCode.length;
+
+  if (isRoundFinish) {
+    // TODO: 모든 코드 제출을 통해 한 라운드가 끝났음을 서버에 발행
+    // TODO: 시간 종료를 통해 한 라운드가 끝났음을 서버에 발행
+    // console.log('모든 코드 제출!');
   }
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -20,25 +25,25 @@ const CodeForm = ({ convertedDummyCode }: CodeFormProps) => {
     return currentInput === convertedDummyCode[currentIndex];
   };
 
-  const handleActiveEnter = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      const isCorrectCode = handleCheckCorrect(currentInputValue);
-      // console.log('isCorrectCode', isCorrectCode);
-      if (isCorrectCode) {
-        // TODO: enter키 누르면 publish
-        // TODO: 다음 문장 입력되도록 currentIndex 증가
-        setCurrentIndex((prev) =>
-          prev < convertedDummyCode.length ? prev + 1 : prev
-        );
+  const handleActiveEnter = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        setCurrentInputValue('');
-        // console.log('정답 제출!');
-      }
+    const isCorrectCode = handleCheckCorrect(currentInputValue);
+    // console.log('isCorrectCode', isCorrectCode);
+    if (isCorrectCode === false || isRoundFinish) {
+      return;
     }
+    // TODO: enter키 누르면 실시간 점수 publish
+    setCurrentIndex((prev) =>
+      prev < convertedDummyCode.length ? prev + 1 : prev
+    );
+
+    setCurrentInputValue('');
+    // console.log('정답 제출!');
   };
 
   return (
-    <>
+    <form onSubmit={handleActiveEnter}>
       <input
         autoFocus
         type='text'
@@ -46,12 +51,16 @@ const CodeForm = ({ convertedDummyCode }: CodeFormProps) => {
 bg-white border-2 border-green-100 my-4
 outline-0 text-gray-300 tracking-wider box-border`}
         value={currentInputValue}
-        placeholder={convertedDummyCode[currentIndex]}
-        maxLength={convertedDummyCode[currentIndex].length}
+        placeholder={
+          isRoundFinish
+            ? '라운드가 끝났습니다!'
+            : convertedDummyCode[currentIndex]
+        }
+        maxLength={isRoundFinish ? 0 : convertedDummyCode[currentIndex].length}
+        disabled={isRoundFinish ? true : false}
         onChange={handleInputChange}
-        onKeyDown={handleActiveEnter}
       />
-    </>
+    </form>
   );
 };
 export default CodeForm;
