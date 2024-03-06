a4ae19fc692bd1ba6db2652811ca8690d602137d
yejinleee <yejin9487@gmail.com>
2024-03-06 15:25:44 +0900

    [♻️refactor]: store에 단어100개 저장 로직 제거

--- src/pages/GamePage/GameWord/GameWord.tsx
+++ src/pages/GamePage/GameWord/GameWord.tsx
@@ -2,8 +2,6 @@ import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import Dashboard from '@/common/Ingame/Dashboard';
 import IngameHeader from '@/common/Ingame/IngameHeader';
-import useWordsStore from '@/store/useWordsStore';
-import { checkIsEmptyObj } from '@/utils/checkIsEmptyObj';
 import { I_IngameWsResponse, PayloadType } from '../types/websocketType';
 import WordCell from './WordCell';
 import WordRankContainer from './WordRankContainer';
@@ -20,26 +18,13 @@ const GameWord = ({
   userId: number;
 }) => {
   const { register, handleSubmit, setValue, getValues } = useForm();
-  const { wordsStore, setWordsStore, setSubmittedWord } = useWordsStore();
 
   useEffect(() => {
     if (ingameRoomRes.submittedWord) {
       const submittedWord: WordQuestionType = {};
       submittedWord[ingameRoomRes.submittedWord] = -1;
-      setSubmittedWord(submittedWord);
     }
   }, [ingameRoomRes.submittedWord]);
-
-  useEffect(() => {
-    if (!checkIsEmptyObj(wordsStore)) {
-      return;
-    }
-    const words: WordQuestionType = {};
-    for (const idx in ingameRoomRes.questions) {
-      words[ingameRoomRes.questions[Number(idx)].question] = Number(idx);
-    }
-    setWordsStore(words);
-  }, [ingameRoomRes.questions]);
 
   return (
     <>
@@ -47,16 +32,15 @@ const GameWord = ({
       <div className='grow'>
         <div className='flex flex-col items-center justify-around h-[60rem]'>
           <div className='h-[25rem] grid grid-rows-[repeat(8,minmax(0,1fr))] grid-cols-[repeat(14,9rem)] text-[1.6rem] p-4 box-content bg-gray-10 rounded-2xl'>
-            {!checkIsEmptyObj(wordsStore) &&
-              Object.entries(wordsStore).map(([word, wordIdx], i) => {
-                return (
-                  <WordCell
-                    key={i}
-                    randomIndex={i % 3}>
-                    {wordIdx >= 0 ? word : ' '}
-                  </WordCell>
-                );
-              })}
+            {ingameRoomRes.questions?.map((w, i) => {
+              return (
+                <WordCell
+                  key={i}
+                  randomIndex={i % 3}>
+                  {w.question.startsWith('#') ? ' ' : w.question}
+                </WordCell>
+              );
+            })}
           </div>
           <div className='flex flex-row items-end w-10/12 h-[30rem] mt-4'>
             <Dashboard
