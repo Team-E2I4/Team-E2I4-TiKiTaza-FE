410ae6c079d76332c45e83c081059908a24961d2
dlsxjzld <sinjuk1@naver.com>
2024-03-06 15:19:34 +0900

    [✨feat]: 문장 입력할때 맞게 입력한 것은 녹색 및 bold 처리, 오타는 붉은색 처리

--- src/pages/GamePage/GameCode/CodeForm.tsx
+++ src/pages/GamePage/GameCode/CodeForm.tsx
@@ -1,15 +1,31 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useMemo, useState } from 'react';
 
 interface CodeFormProps {
   convertedDummyCode: string[];
 }
+
+const CHAR_STATE = {
+  PENDING: 'pending',
+  CORRECT: 'correct',
+  TYPO: 'typo',
+};
 
 const CodeForm = ({ convertedDummyCode }: CodeFormProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentInputValue, setCurrentInputValue] = useState('');
   // const convertedDummyCode = ['ho', 'hi'];
   // console.log('convertedDummyCode', convertedDummyCode);
+
   const isRoundFinish = currentIndex === convertedDummyCode.length;
+
+  const checkedCorrectAndTypo = useMemo(
+    () =>
+      Array.from(
+        { length: convertedDummyCode[currentIndex].length },
+        () => CHAR_STATE.PENDING
+      ),
+    [convertedDummyCode, currentIndex]
+  );
 
   if (isRoundFinish) {
     // TODO: 모든 코드 제출을 통해 한 라운드가 끝났음을 서버에 발행
@@ -18,7 +34,41 @@ const CodeForm = ({ convertedDummyCode }: CodeFormProps) => {
   }
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setCurrentInputValue(e.target.value);
+    const currentTypingInput = e.target.value;
+    setCurrentInputValue(() => currentTypingInput);
+    handleCheckCorrectAndTypo(currentTypingInput);
+  };
+
+  const handleCheckCorrectAndTypo = (currentTypingInput: string) => {
+    // 현재 입력하고 있는 값의 가장 마지막 위치
+    if (currentTypingInput === '') {
+      return;
+    }
+
+    const currentCharIndex = currentTypingInput.length;
+    const slicedCurrentCode = convertedDummyCode[currentIndex].slice(
+      0,
+      currentCharIndex
+    );
+
+    let isTypoExist = false;
+    Array(...currentTypingInput).forEach((currentInputChar, idx) => {
+      if (
+        currentInputChar === slicedCurrentCode[idx] &&
+        isTypoExist === false
+      ) {
+        checkedCorrectAndTypo[idx] = CHAR_STATE.CORRECT;
+      } else {
+        checkedCorrectAndTypo[idx] = CHAR_STATE.TYPO;
+        isTypoExist = true;
+      }
+    });
+
+    checkedCorrectAndTypo.forEach((_, idx) => {
+      if (idx >= currentCharIndex) {
+        checkedCorrectAndTypo[idx] = CHAR_STATE.PENDING;
+      }
+    });
   };
 
   const handleCheckCorrect = (currentInput: string) => {
@@ -33,6 +83,7 @@ const CodeForm = ({ convertedDummyCode }: CodeFormProps) => {
     if (isCorrectCode === false || isRoundFinish) {
       return;
     }
+
     // TODO: enter키 누르면 실시간 점수 publish
     setCurrentIndex((prev) =>
       prev < convertedDummyCode.length ? prev + 1 : prev
@@ -43,24 +94,40 @@ const CodeForm = ({ convertedDummyCode }: CodeFormProps) => {
   };
 
   return (
-    <form onSubmit={handleActiveEnter}>
-      <input
-        autoFocus
-        type='text'
-        className={`w-[60rem] h-[4rem] flex items-center pl-[1.75rem] rounded-2xl
-bg-white border-2 border-green-100 my-4
-outline-0 text-gray-300 tracking-wider box-border`}
-        value={currentInputValue}
-        placeholder={
-          isRoundFinish
-            ? '라운드가 끝났습니다!'
-            : convertedDummyCode[currentIndex]
-        }
-        maxLength={isRoundFinish ? 0 : convertedDummyCode[currentIndex].length}
-        disabled={isRoundFinish ? true : false}
-        onChange={handleInputChange}
-      />
-    </form>
+    <>
+      <div
+        className='w-[60rem] h-[4rem] flex items-center pl-[1.75rem] rounded-2xl
+        bg-white border-2 border-green-100 my-4
+        outline-0 text-gray-300 tracking-wider box-border'>
+        {[...convertedDummyCode[currentIndex]].map((char, idx) => (
+          <span
+            className={`${checkedCorrectAndTypo[idx] === CHAR_STATE.CORRECT ? 'text-green-600 font-bold' : checkedCorrectAndTypo[idx] === CHAR_STATE.TYPO ? 'text-red-500' : 'text-black'}`}
+            key={`${checkedCorrectAndTypo[idx]}${idx}`}>
+            {char === ' ' ? '\u00A0' : char}
+          </span>
+        ))}
+      </div>
+      <form onSubmit={handleActiveEnter}>
+        <input
+          autoFocus
+          type='text'
+          className={`w-[60rem] h-[4rem] flex items-center pl-[1.75rem] rounded-2xl
+        bg-white border-2 border-green-100 my-4
+        outline-0 text-gray-300 tracking-wider box-border`}
+          value={currentInputValue}
+          placeholder={
+            isRoundFinish
+              ? '라운드가 끝났습니다!'
+              : convertedDummyCode[currentIndex]
+          }
+          maxLength={
+            isRoundFinish ? 0 : convertedDummyCode[currentIndex].length
+          }
+          disabled={isRoundFinish ? true : false}
+          onChange={handleInputChange}
+        />
+      </form>
+    </>
   );
 };
 export default CodeForm;
