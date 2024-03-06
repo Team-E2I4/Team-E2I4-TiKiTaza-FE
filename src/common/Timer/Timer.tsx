446467c138fbdddf19da1bece918c982a059ca1c
Baek <woonn97@gmail.com>
2024-03-06 13:09:18 +0900

    [✨feat]: 코드리뷰 반영

--- src/common/Timer/Timer.tsx
+++ src/common/Timer/Timer.tsx
@@ -18,20 +18,21 @@ const convertTime = (time: number) => {
 };
 
 const Timer = ({ minutes = 0, seconds = 0, onFinishRound }: TimerProps) => {
-  const timer = useRef<NodeJS.Timeout | null>(null);
+  const timer = useRef<ReturnType<typeof setInterval> | null>(null);
   const [timeLeft, setTimeLeft] = useState<number>(
     minutes * MIN_MS + seconds * MS
   );
 
   useEffect(() => {
-    if (timeLeft <= 0) {
-      if (timer.current != null) {
-        clearInterval(timer.current);
-      }
-      setTimeLeft(0);
-      onFinishRound(); //TODO: 타이머 끝난 후 진행하는 이벤트
-      alert('라운드 종료!');
+    if (timeLeft > 0) {
+      return;
     }
+    if (timer.current !== null) {
+      clearInterval(timer.current);
+    }
+    setTimeLeft(0);
+    onFinishRound(); //TODO: 타이머 끝난 후 진행하는 이벤트
+    alert('라운드 종료!');
   }, [timeLeft, onFinishRound]);
 
   useEffect(() => {
@@ -40,15 +41,17 @@ const Timer = ({ minutes = 0, seconds = 0, onFinishRound }: TimerProps) => {
     }, MS);
 
     return () => {
-      if (timer.current != null) {
+      if (timer.current !== null) {
         clearInterval(timer.current);
       }
     };
   }, []);
 
   return (
-    <div className='font-bold font-[Giants-Inline] text-[3.2rem]'>
-      <h1>남은 시간 : {convertTime(timeLeft)}</h1>
+    <div>
+      <span className='font-bold font-[Giants-Inline] text-[3.2rem]'>
+        남은 시간 : {convertTime(timeLeft)}
+      </span>
     </div>
   );
 };
