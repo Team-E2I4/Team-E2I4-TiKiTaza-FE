728a26f4d11b17daebffbcb0c13480bf189d1bb8
loevray <rei980213@gmail.com>
2024-03-07 14:22:54 +0900

    [🔨chore]: 로컬에서도 https사용할 수 있게 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+  server:{https:true},
   define: {
     'global': {},
   },
