7ebe8f552ed51f92dfd7eaecb19016e849437d35
loevray <rei980213@gmail.com>
2024-03-07 11:40:46 +0900

    [♻️refactor]: Input컴포넌트 타입 추가 및 forwardRef추가

--- src/common/Input/Input.tsx
+++ src/common/Input/Input.tsx
@@ -1,15 +1,26 @@
-interface InputProps {
+import { ForwardedRef, forwardRef } from 'react';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   whSize?: string;
 }
-const Input = ({ whSize = `w-[70rem] h-[4.5rem]` }: InputProps) => {
-  return (
-    <>
-      <input
-        className={`${whSize} flex items-center pl-[1.75rem] rounded-2xl
+const Input = forwardRef(
+  (
+    { whSize = `w-[70rem] h-[4.5rem]`, ...props }: InputProps,
+    ref?: ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <>
+        <input
+          ref={ref}
+          {...props}
+          className={`${whSize} flex items-center pl-[1.75rem] rounded-2xl
 bg-white border-2 border-green-100 my-4
 outline-0 text-gray-300 tracking-wider box-border`}
-      />
-    </>
-  );
-};
+        />
+      </>
+    );
+  }
+);
+
+Input.displayName = 'InputComponent';
 export default Input;
