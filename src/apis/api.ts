6b686804b3e3f5dd1a890f577152d29e618cac43
loevray <rei980213@gmail.com>
2024-03-07 13:51:20 +0900

    [✨feat]: 404-01에러(토큰만료)시 토큰 재발급 받게 intercept(미완성)

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -5,12 +5,36 @@ import storageFactory from '@/utils/storageFactory';
 
 const { getItem } = storageFactory(localStorage);
 
+async function refreshToken() {
+  /*   const response = await reIssueAccessToken();
+  return response.data.data?.accessToken; */
+}
+
 //임시로 MyToken이라는 값을 정해두었습니다
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+  withCredentials: true,
+});
+
 axiosInstance.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${getItem('MyToken', '')}`;
   return config;
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originRequest = error.config;
+    const errorCode = error.response.data.errorCode;
+    if (errorCode === 'mem-404/01' && !originRequest._retry) {
+      originRequest._retry = true;
+      await refreshToken();
+      return axiosInstance;
+    }
+    return Promise.reject(error);
+  }
+);
 
 export const {
   createGameRoom,
