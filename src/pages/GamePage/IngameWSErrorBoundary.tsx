ba4966afc9cc7b1a9db59a67ddb129984faedab0
loevray <rei980213@gmail.com>
2024-03-07 11:43:19 +0900

    [♻️refactor]: publishIngame타입 네이밍 추가

--- src/pages/GamePage/IngameWSErrorBoundary.tsx
+++ src/pages/GamePage/IngameWSErrorBoundary.tsx
@@ -1,8 +1,16 @@
 import useRoomInfoStore from '@/store/useRoomInfoStore';
 import WsError from './common/WsError';
+import useIngameWebsocket, { PayloadType } from './hooks/useIngameWebsocket';
+import { I_IngameWsResponse } from './types/websocketType';
+
+export type PublishIngameType = (
+  destination: string,
+  payload: PayloadType
+) => void;
+
 export interface InagmeWsChildrenProps {
   ingameRoomRes: I_IngameWsResponse;
-  publishIngame: (destination: string, payload: PayloadType) => void;
+  publishIngame: PublishIngameType;
 }
 import useIngameWebsocket from './hooks/useIngameWebsocket';
 import { I_IngameWsResponse, PayloadType } from './types/websocketType';
