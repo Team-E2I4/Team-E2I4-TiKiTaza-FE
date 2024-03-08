64ac1857dd59237a41af2a2f9bae31148b64a6bd
loevray <rei980213@gmail.com>
2024-03-08 22:58:29 +0900

    [✨feat]: 인게임 랭킹로직 추가

--- src/common/Ingame/IngameRank.tsx
+++ src/common/Ingame/IngameRank.tsx
@@ -1,48 +1,6 @@
-// TODO: 서버로부터 받는 데이터 구조에 따라 props가 바뀌어야 할 것 같습니다.
-interface IEachRankInfo {
-  isMe: boolean;
-  rank?: number; // 인덱스가 순위 정보이지 않을까?
-  name: string;
-}
-interface IngameRankProps {
-  rankInfos?: IEachRankInfo[];
-}
+import { I_RankInfoList } from '@/pages/GamePage/GameSentence/GameSentence';
 
-const rankInfosDummy = [
-  {
-    isMe: false,
-    name: '유저11',
-  },
-  {
-    isMe: true,
-    name: '나야나',
-  },
-  {
-    isMe: false,
-    name: '유저222',
-  },
-  {
-    isMe: false,
-    name: '유저3',
-  },
-  {
-    isMe: false,
-    name: '유저44444444444444444',
-  },
-  {
-    isMe: false,
-    name: '유저5',
-  },
-  {
-    isMe: false,
-    name: '유저6',
-  },
-  {
-    isMe: false,
-    name: '유저777777_7_&',
-  },
-];
-const IngameRank = ({ rankInfos = rankInfosDummy }: IngameRankProps) => {
+const IngameRank = ({ rankInfos }: { rankInfos: I_RankInfoList[] }) => {
   return (
     <>
       {rankInfos.map((rankInfo, i) => {
@@ -51,14 +9,14 @@ const IngameRank = ({ rankInfos = rankInfosDummy }: IngameRankProps) => {
             key={i}
             className={`pl-4 flex bg-gradient-to-r items-center gap-4 mb-1 ${rankInfo.isMe ? 'from-green-100 h-[5rem] w-[30rem]' : 'from-gray-200 h-[4rem] w-[20rem]'}`}>
             <div className={rankInfo.isMe ? 'text-[5rem]' : 'text-[4rem]'}>
-              {rankInfo.rank}
+              {i + 1}
             </div>
             <div className={rankInfo.isMe ? 'text-[3rem]' : 'text-[2rem]'}>
               🚗
             </div>
             <div
               className={`truncate ${rankInfo.isMe ? 'text-[3rem]' : 'text-[2rem]'}`}>
-              {rankInfo.name}
+              {rankInfo.nickname}
             </div>
           </div>
         );
