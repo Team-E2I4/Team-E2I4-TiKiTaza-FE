95d0dcc22d9f23980d675d3cbf95b95d81a5ba65
yejinleee <yejin9487@gmail.com>
2024-03-11 22:58:30 +0900

    [✨feat]: 단어게임 인원수별 타이머 시간 설정, 단어 모두 입력시 다음라운드

--- src/pages/GamePage/GameWord/GameWord.tsx
+++ src/pages/GamePage/GameWord/GameWord.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
 import IngameHeader from '@/common/Ingame/IngameHeader';
 import { checkIsEmptyObj } from '@/utils/checkIsEmptyObj';
 import { InagmeWsChildrenProps } from '../IngameWSErrorBoundary';
@@ -8,9 +9,33 @@ interface GameWordProps extends InagmeWsChildrenProps {
 }
 
 const GameWord = ({ ingameRoomRes, publishIngame, userId }: GameWordProps) => {
+  const [currentRound, setCurrentRound] = useState(1);
+  const didRoundFinishSubmitted = useRef(false);
+
+  useEffect(() => {
+    if (ingameRoomRes.type === 'NEXT_ROUND_START') {
+      setCurrentRound((prev) => prev + 1);
+      didRoundFinishSubmitted.current = false;
+      return;
+    }
+  }, [ingameRoomRes.type]);
+
+  const handleRoundFinish = useCallback(() => {
+    if (didRoundFinishSubmitted.current) {
+      return;
+    }
+    publishIngame('/round-finish', { currentRound });
+    didRoundFinishSubmitted.current = true;
+  }, [currentRound, publishIngame]);
+
   return (
     <>
-      <IngameHeader />
+      <IngameHeader
+        handleRoundFinish={handleRoundFinish}
+        currentRound={currentRound}
+        timeLimit={Math.ceil(120 / ingameRoomRes.allMembers.length)}
+        isNextRound={ingameRoomRes.type === 'NEXT_ROUND_START'}
+      />
       <div className='grow'>
         <div className='flex flex-col items-center justify-between h-[61rem]'>
           {!checkIsEmptyObj(ingameRoomRes) && (
@@ -18,6 +43,7 @@ const GameWord = ({ ingameRoomRes, publishIngame, userId }: GameWordProps) => {
               ingameRoomRes={ingameRoomRes}
               publishIngame={publishIngame}
               userId={userId}
+              handleRoundFinish={handleRoundFinish}
             />
           )}
         </div>
