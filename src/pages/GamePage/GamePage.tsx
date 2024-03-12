9ff43c675b788d1b13a24a571a6196cf970ce13a
loevray <rei980213@gmail.com>
2024-03-12 09:23:35 +0900

    [🗑️remove]: GamePage에서 안쓰이는 import 제거

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,24 +1,20 @@
-import { Fragment, useEffect, useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 import Loading from '@/common/Loading/Loading';
 import { useAuthCheck } from '@/hooks/useAuth';
 import useRoomInfoStore from '@/store/useRoomInfoStore';
 import { checkIsEmptyObj } from '@/utils/checkIsEmptyObj';
 import WsError from './common/WsError';
-// eslint-disable-next-line unused-imports/no-unused-imports, @typescript-eslint/no-unused-vars
 import GameCode from './GameCode/GameCode';
 import GameFinish from './GameFinish/GameFinish';
-// eslint-disable-next-line unused-imports/no-unused-imports, @typescript-eslint/no-unused-vars
 import GameSentence from './GameSentence/GameSentence';
 import GameWaitingRoom from './GameWaitingRoom/GameWaitingRoom';
-// eslint-disable-next-line unused-imports/no-unused-imports, @typescript-eslint/no-unused-vars
 import GameWord from './GameWord/GameWord';
 import useWebsocket from './hooks/useWebsocket';
 import { IngameWSErrorBoundary } from './IngameWSErrorBoundary';
 
 const GamePage = () => {
   // TODO: 초대로 들어온 사람이라면 url의 해시값->정제->유효검사 후 상태값) 으로 방번호 추출
-
   const { roomId, setRoomInfo, roomInfo } = useRoomInfoStore();
 
   const {
