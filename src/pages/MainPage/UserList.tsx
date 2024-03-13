ac53a1ed14b62fb56d713521482d32c9fd239b52
loevray <rei980213@gmail.com>
2024-03-13 15:40:37 +0900

    [✨feat]: 유저 목록에서 본인임을 나타내는 표시 추가

--- src/pages/MainPage/UserList.tsx
+++ src/pages/MainPage/UserList.tsx
@@ -1,8 +1,18 @@
+import { useAuthCheck } from '@/hooks/useAuth/useAuth';
 import useOnlineUsers from '@/hooks/useOnlineUsers';
 import UserListItem from './UserListItem';
 
 const UserList = () => {
   const { data: userList } = useOnlineUsers();
+  const { data: userData, isPending, error } = useAuthCheck();
+
+  if (isPending) {
+    return <div>유저 정보 불러오는중...</div>;
+  }
+
+  if (error) {
+    return <div>유저 정보 불러오는 중 에러</div>;
+  }
 
   return (
     <article className='flex flex-col gap-[1rem] p-[2rem] bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[50rem] w-full'>
@@ -12,7 +22,11 @@ const UserList = () => {
           [...userList.data.data].map(({ nickname, memberId }) => (
             <UserListItem
               key={memberId}
-              username={nickname}
+              username={
+                memberId == userData.data.data?.memberId
+                  ? `${nickname} (본인)`
+                  : `${nickname}`
+              }
             />
           ))
         ) : (
