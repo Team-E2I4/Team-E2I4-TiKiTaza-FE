ae4c7bf30c20e982642f432e5dd40a6ceedb631e
Baek <woonn97@gmail.com>
2024-03-13 20:24:09 +0900

    [✨feat]: mm:ss:ff 단위로 타이머되도록 수정

--- src/common/Timer/utils/convertTime.ts
+++ src/common/Timer/utils/convertTime.ts
@@ -1,9 +1,9 @@
-import { MS } from '@/hooks/useTimer';
+import { SEC_MS } from '@/hooks/useTimer';
 
 export const convertTime = (time: number) => {
-  const minutes = Math.floor(time / MS / 60); // 분
-  const seconds = Math.floor((time / MS) % 60); // 초
-  const frames = Math.floor((time % MS) / 10); // 프레임
+  const minutes = Math.floor(time / SEC_MS / 60); // 분
+  const seconds = Math.floor((time / SEC_MS) % 60); // 초
+  const frames = Math.floor((time % SEC_MS) / 10); // 프레임
 
   const MMSS = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`; // MM:SS
   const MMSSFF = `${MMSS}:${String(frames).padStart(2, '0')}`; // MM:SS:FF
