7a1243a86f1bb3493a8d794ecc4fa8ffcd6f0fba
loevray <rei980213@gmail.com>
2024-03-13 17:06:02 +0900

    [✨feat]: 유저 프로필에 각종 유저 정보 추가

--- src/pages/MainPage/UserCard.tsx
+++ src/pages/MainPage/UserCard.tsx
@@ -1,27 +1,37 @@
 import * as Avatar from '@radix-ui/react-avatar';
 
 interface UserCardProps {
+  nickname: string;
   userImage?: string;
   userImageFallbackDelay?: number;
   rank?: number;
+  isGuest: boolean;
+  gameCount: number;
+  averageCpm: number;
+  averageAccuracy: number;
 }
 
 const UserCard = ({
   //테스트용으로 기본값 추가해두었습니다.
+  nickname,
   userImage = 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80',
   userImageFallbackDelay = 6000,
-  rank = 0,
+  rank,
+  isGuest,
+  gameCount = 0,
+  averageCpm = 0,
+  averageAccuracy = 0,
 }: UserCardProps) => {
   return (
     <article className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[18rem] w-full p-[1.2rem]'>
       <div className='flex justify-between'>
-        <span className='truncate flex-1'>
-          닉네임이겁나길수도있습니다이렇게
-        </span>
-        <button>(수정)</button>
+        <span className='truncate flex-1'>{nickname}</span>
+        {!isGuest && (
+          <button className='w-[6rem] h-[2.5rem] bg-red-100'>수정</button>
+        )}
       </div>
-      <div className='py-[2.2rem] flex'>
-        <Avatar.Root className='w-1/2'>
+      <div className='pb-[2.2rem] flex'>
+        <Avatar.Root className='w-1/2 pt-[2.2rem]'>
           <Avatar.Image
             className='size-[9rem] rounded-full'
             src={userImage}
@@ -31,8 +41,13 @@ const UserCard = ({
             테스트
           </Avatar.Fallback>
         </Avatar.Root>
-        <div className='flex flex-col-reverse  mx-[1.2rem]'>
-          <span className='bg-coral-50 w-[6.8rem] text-center'>{`${rank ? `${rank}위` : '순위없음'}`}</span>
+
+        {/* Todo: 각 정보 눌렀을때 랭크페이지 이동? */}
+        <div className='flex flex-col-reverse  mx-[1.2rem] gap-[1rem] text-[1.4rem]'>
+          <span className='bg-coral-50 w-[10rem] text-center rounded-[0.5rem] h-[2.2rem] hover:bg-coral-100'>{`${!isGuest ? `${rank}위` : '순위없음'}`}</span>
+          <span className='bg-coral-50 w-[10rem] text-center rounded-[0.5rem] h-[2.2rem] hover:bg-coral-100'>{`플레이 ${gameCount}회`}</span>
+          <span className='bg-coral-50 w-[10rem] text-center rounded-[0.5rem] h-[2.2rem] hover:bg-coral-100'>{`타수 ${averageCpm}타`}</span>
+          <span className='bg-coral-50 w-[10rem] text-center rounded-[0.5rem] h-[2.2rem] hover:bg-coral-100'>{`정확도 ${averageAccuracy}%`}</span>
         </div>
       </div>
     </article>
