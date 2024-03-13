5e7e3564bde1737dc90d94dc88a11891a6817251
dlsxjzld <sinjuk1@naver.com>
2024-03-13 20:46:37 +0900

    [✨feat]: 로직에 필요한 상태를 store에 추가

--- src/store/useGameWaitingRoomStore.ts
+++ src/store/useGameWaitingRoomStore.ts
@@ -1,11 +1,20 @@
 import { create } from 'zustand';
-import { I_GameRoomResponse } from '@/pages/GamePage/types/websocketType';
+import {
+  I_AllMember,
+  I_GameRoomResponse,
+} from '@/pages/GamePage/types/websocketType';
 
 interface I_UseGameWaitingRoom {
   gameRoomRes: I_GameRoomResponse | null;
   setGameRoomRes: (gameRoomRes: I_GameRoomResponse) => void;
   isWsError: boolean;
   setIsWsError: (isWsError: boolean) => void;
+
+  didAdminStart: boolean;
+  setDidAdminStart: (didAdminStart: boolean) => void;
+
+  allMembers: I_AllMember[] | undefined;
+  setAllMembers: (allMembers: I_AllMember[]) => void;
 }
 
 const useGameWaitingRoomStore = create<I_UseGameWaitingRoom>((set) => ({
@@ -13,6 +22,12 @@ const useGameWaitingRoomStore = create<I_UseGameWaitingRoom>((set) => ({
   setGameRoomRes: (gameRoomRes) => set({ gameRoomRes }),
   isWsError: false,
   setIsWsError: (isWsError) => set({ isWsError }),
+
+  didAdminStart: false,
+  setDidAdminStart: (didAdminStart) => set({ didAdminStart }),
+
+  allMembers: undefined,
+  setAllMembers: (allMembers) => set({ allMembers }),
 }));
 
 export default useGameWaitingRoomStore;
