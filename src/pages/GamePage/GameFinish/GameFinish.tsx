942f1649250ea75f88b60234737c854b3f1165fb
dlsxjzld <sinjuk1@naver.com>
2024-03-13 20:47:54 +0900

    [✨feat]: 라운드 종료 후 다시 게임 대기방으로 갈 수 있도록 하는 로직

--- src/pages/GamePage/GameFinish/GameFinish.tsx
+++ src/pages/GamePage/GameFinish/GameFinish.tsx
@@ -1,20 +1,35 @@
 import { useNavigate } from 'react-router-dom';
 import { RankingResponse } from '@/generated';
 import useTimer from '@/hooks/useTimer';
+import useGameWaitingRoomStore from '@/store/useGameWaitingRoomStore';
+import useIngameStore from '@/store/useIngameStore';
 import RankList from '../../RankPage/RankList';
+import { I_IngameWsResponse } from '../types/websocketType';
 
 const GameFinish = ({ rankData }: { rankData: RankingResponse[] }) => {
   const navigate = useNavigate();
+  const { setDidAdminStart } = useGameWaitingRoomStore();
+  const { setIngameRoomRes } = useIngameStore();
+
   const { timeLeft } = useTimer({
     minutes: 0,
     seconds: 4,
-    onFinishRound: () => navigate('/main', { replace: true }),
+    onFinishRound: () => {
+      onMoveToGameRoom();
+    },
   });
+
+  const onMoveToGameRoom = () => {
+    setDidAdminStart(false);
+    setIngameRoomRes({} as I_IngameWsResponse);
+    navigate('/game', { replace: true });
+  };
+
   return (
     <div className='flex flex-col gap-[8rem] items-center'>
       <section>
         <span className='text-[3rem] font-bold font-[Giants-Inline]'>
-          {`${timeLeft}초 뒤 메인화면으로 이동합니다.`}
+          {`${timeLeft}초 뒤 게임 대기방으로 이동합니다.`}
         </span>
       </section>
       <section className='flex gap-[10rem] w-full'>
@@ -53,10 +68,19 @@ const GameFinish = ({ rankData }: { rankData: RankingResponse[] }) => {
         </div>
       </section>
       <section className='flex gap-10 font-[Giants-Inline]'>
-        <button className='bg-coral-100 px-6 py-6 rounded-[1rem] text-[3rem] font-bold'>
+        <button
+          onClick={() => {
+            navigate('/main', { replace: true });
+            navigate(0);
+          }}
+          className='bg-coral-100 px-6 py-6 rounded-[1rem] text-[3rem] font-bold'>
           로비로 나가기
         </button>
-        <button className='bg-coral-100 px-6 py-6 rounded-[1rem] text-[3rem] font-bold'>
+        <button
+          onClick={() => {
+            onMoveToGameRoom();
+          }}
+          className='bg-coral-100 px-6 py-6 rounded-[1rem] text-[3rem] font-bold'>
           다시하기
         </button>
       </section>
