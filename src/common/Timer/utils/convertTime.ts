2bc1e159f385fbeae2d2f15d1ace2c45933e974b
Baek <woonn97@gmail.com>
2024-03-13 20:12:44 +0900

    [✨feat]: covertTime 로직 수정

--- src/common/Timer/utils/convertTime.ts
+++ src/common/Timer/utils/convertTime.ts
@@ -1,9 +1,11 @@
 import { MS } from '@/hooks/useTimer';
 
 export const convertTime = (time: number) => {
-  return (
-    String(Math.floor(time / MS / 60)).padStart(2, '0') +
-    ':' +
-    String((time / MS) % 60).padStart(2, '0')
-  );
+  const minutes = Math.floor(time / MS / 60); // 분
+  const seconds = Math.floor((time / MS) % 60); // 초
+  const frames = Math.floor((time % MS) / 10); // 프레임
+
+  const MMSS = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`; // MM:SS
+  const MMSSFF = `${MMSS}:${String(frames).padStart(2, '0')}`; // MM:SS:FF
+  return { MMSS, MMSSFF };
 };
