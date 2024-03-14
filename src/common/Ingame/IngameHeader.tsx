607110e949f53de768919b5af9ae3cf1751ff824
loevray <rei980213@gmail.com>
2024-03-14 10:28:22 +0900

    [🐛fix]: 타이머값 mm:ss로 변경하여 사용

--- src/common/Ingame/IngameHeader.tsx
+++ src/common/Ingame/IngameHeader.tsx
@@ -3,6 +3,7 @@ import useTimer from '@/hooks/useTimer';
 import DisconnectModal from '@/pages/GamePage/common/DisconnectModal';
 import useRoomInfoStore from '@/store/useRoomInfoStore';
 import Backward from '../Backward/Backward';
+import { convertTime } from '../Timer/utils/convertTime';
 
 interface IngameHeaderProps {
   handleRoundFinish: () => void;
@@ -51,7 +52,7 @@ const IngameHeader = ({
         <div className='grow'>참여 {roomInfo?.currentPlayer}명</div>
         <div>
           <span className='font-bold font-[Giants-Inline] text-[3.2rem]'>
-            남은 시간 : {timeLeft}
+            남은 시간 : {convertTime(timeLeft)}
           </span>
         </div>
         <div className='text-[3rem]'>
