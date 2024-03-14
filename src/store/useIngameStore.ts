ebad3b0c5595488396307518a58f14d691f97cb7
loevray <rei980213@gmail.com>
2024-03-14 11:48:59 +0900

    [✨feat]: 라운드 시작 전 대기시간용 모달 추가

--- src/store/useIngameStore.ts
+++ src/store/useIngameStore.ts
@@ -4,15 +4,23 @@ import { I_IngameWsResponse } from '@/pages/GamePage/types/websocketType';
 interface I_useIngameStore {
   ingameRoomRes: I_IngameWsResponse;
   setIngameRoomRes: (ingameRoomRes: I_IngameWsResponse) => void;
+
   isIngameWsError: boolean;
   setIsIngameWsError: (isIngameWsError: boolean) => void;
+
+  isRoundWaiting: boolean;
+  setIsRoundWaiting: (isRoundWaiting: boolean) => void;
 }
 
 const useIngameStore = create<I_useIngameStore>((set) => ({
   ingameRoomRes: {} as I_IngameWsResponse,
   setIngameRoomRes: (ingameRoomRes) => set({ ingameRoomRes }),
+
   isIngameWsError: false,
   setIsIngameWsError: (isIngameWsError) => set({ isIngameWsError }),
+
+  isRoundWaiting: false,
+  setIsRoundWaiting: (isRoundWaiting) => set({ isRoundWaiting }),
 }));
 
 export default useIngameStore;
