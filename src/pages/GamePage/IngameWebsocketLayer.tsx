9cebd84db5371d4e63b7e329381049b898b682b1
loevray <rei980213@gmail.com>
2024-03-14 12:40:39 +0900

    [✨feat]: 라운드 별 대기시간 추가

--- src/pages/GamePage/IngameWebsocketLayer.tsx
+++ src/pages/GamePage/IngameWebsocketLayer.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import useIngameStore from '@/store/useIngameStore';
 import useRoomInfoStore from '@/store/useRoomInfoStore';
 import { checkIsEmptyObj } from '@/utils/checkIsEmptyObj';
+import RoundWaitModal from './common/RoundWaitModal';
 import WsError from './common/WsError';
 import GameCode from './GameCode/GameCode';
 import GameFinish from './GameFinish/GameFinish';
@@ -22,12 +23,22 @@ const IngameWebsocketLayer = ({
   handleConnectIngame,
 }: IngameWebsocketLayerProps) => {
   const { roomId, roomInfo } = useRoomInfoStore();
-  const { ingameRoomRes, isIngameWsError } = useIngameStore();
+  const { ingameRoomRes, isIngameWsError, isRoundWaiting, setIsRoundWaiting } =
+    useIngameStore();
 
   useEffect(() => {
     onIngameConnected(); // 인게임 엔드포인트 구독
     handleConnectIngame(roomId); // 해당 인게임 연결 발행
   }, []);
+
+  useEffect(() => {
+    if (
+      ingameRoomRes.type === 'FIRST_ROUND_START' ||
+      ingameRoomRes.type === 'NEXT_ROUND_START'
+    ) {
+      setIsRoundWaiting(true);
+    }
+  }, [ingameRoomRes.type]);
 
   if (isIngameWsError || checkIsEmptyObj(ingameRoomRes)) {
     return <WsError />;
@@ -46,8 +57,18 @@ const IngameWebsocketLayer = ({
     return <GameFinish convertedRankData={convertedRankData} />;
   }
 
+  /* 
+  1. 'FIRST_ROUND_START', 'NEXT_ROUND_START'일때 waiting이라는 상태가 true가 됨.
+  2. waiting이 true일 시 타이머와 함께 modal 등장.
+  3. 타이머가 종료되면, waiting을 false로 만든다.
+  */
+
   return (
     <>
+      <RoundWaitModal
+        isOpen={isRoundWaiting}
+        onTimeFinish={() => setIsRoundWaiting(false)}
+      />
       {roomInfo?.gameMode === 'SENTENCE' && (
         <GameSentence
           publishIngame={publishIngame}
