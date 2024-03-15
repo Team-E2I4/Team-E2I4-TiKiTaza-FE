d6881d25a8fcffec7e7484b0fceac5935b04b9e8
dlsxjzld <sinjuk1@naver.com>
2024-03-15 09:43:12 +0900

    [🎨style]: 나가기 모달 창 콘텐츠, 오버레이 z-index 변경

--- src/pages/GamePage/common/DisconnectModal.tsx
+++ src/pages/GamePage/common/DisconnectModal.tsx
@@ -15,8 +15,8 @@ const DisconnectModal = ({
     <AlertDialog.Root open={isOpen}>
       <AlertDialog.Trigger asChild></AlertDialog.Trigger>
       <AlertDialog.Portal>
-        <AlertDialog.Overlay className='fixed inset-0 w-[100vw] h-[100vh] bg-black/30 animate-[overlayShow_150ms_cubic-bezier(0.16,1,0.3,1)]' />
-        <AlertDialog.Content className='data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-10'>
+        <AlertDialog.Overlay className='fixed inset-0 w-[100vw] h-[100vh] bg-black/30 animate-[overlayShow_150ms_cubic-bezier(0.16,1,0.3,1)] z-[100]' />
+        <AlertDialog.Content className='data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-[100]'>
           <AlertDialog.Title className='text-mauve12 m-0 text-[17px] font-medium'>
             정말로 게임방을 나가시겠어요?
           </AlertDialog.Title>
