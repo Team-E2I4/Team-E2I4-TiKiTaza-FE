1ba0429c4530889c1f9e2d54b0648ac54c22052f
yejinleee <yejin9487@gmail.com>
2024-03-15 09:50:59 +0900

    [🎨style]: 메인페이지 스크롤 없도록 height 변경

--- src/pages/MainPage/UserList.tsx
+++ src/pages/MainPage/UserList.tsx
@@ -8,7 +8,7 @@ interface UserListProps {
 
 const UserList = ({ userList, myId }: UserListProps) => {
   return (
-    <article className='flex flex-col gap-[1rem] p-[2rem] bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[50rem] w-full'>
+    <article className='flex flex-col gap-[1rem] p-[2rem] bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[40rem] w-full'>
       <h3>현재 접속자</h3>
       <ul className='flex-1 rounded-[0.5rem] bg-green-50 p-[2rem] overflow-y-auto'>
         {userList ? (
