6af3cfffce3444162c7cb7b435954ca85253fa1a
yejinleee <yejin9487@gmail.com>
2024-03-15 10:24:12 +0900

    [🎨style]: 배경화면 이미지 추가 - 임시이미지입니다.

--- src/common/Layout/Layout.tsx
+++ src/common/Layout/Layout.tsx
@@ -1,8 +1,13 @@
 import { ReactNode } from 'react';
+import backgroundFlag from '@/assets/backgroundFlag.jpeg';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex flex-col items-center w-100vw min-h-screen bg-layoutBgColor'>
+    <div className='flex flex-col items-center w-100vw min-h-screen'>
+      <img
+        src={backgroundFlag}
+        className='absolute w-full opacity-[0.1] z-[-1] h-screen'
+      />
       {children}
     </div>
   );
