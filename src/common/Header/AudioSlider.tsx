92c8778d10a95c7da628c4050ba3f81748529fea
Baek <woonn97@gmail.com>
2024-03-15 14:48:00 +0900

    [🎨style]: 스타일링 수정

--- src/common/Header/AudioSlider.tsx
+++ src/common/Header/AudioSlider.tsx
@@ -1,4 +1,5 @@
 import * as Slider from '@radix-ui/react-slider';
+import * as Tooltip from '@radix-ui/react-tooltip';
 import { forwardRef } from 'react';
 
 interface AudioSliderProps {
@@ -17,14 +18,27 @@ const AudioSlider = forwardRef<HTMLFormElement, AudioSliderProps>(
           step={1}
           onValueChange={(values) => onChange(values[0])}>
           <Slider.Track className=' bg-white relative grow rounded-[999rem] h-2'>
-            <Slider.Range className='absolute bg-black rounded-[999rem] h-full' />
+            <Slider.Range className='absolute bg-green-300 rounded-[999rem] h-full' />
           </Slider.Track>
-          <Slider.Thumb
-            className='block w-8 h-8 bg-white rounded-full shadow-md shadow-black/50'
-            aria-label='Volume'
-          />
+          <Tooltip.Provider>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <Slider.Thumb
+                  className='block w-8 h-8 bg-white rounded-full shadow-md shadow-black/50'
+                  aria-label='Volume'
+                />
+              </Tooltip.Trigger>
+              <Tooltip.Portal>
+                <Tooltip.Content
+                  className='bg-white p-2 rounded-md shadow-md shadow-black/50 flex items-center justify-center gap-2'
+                  side='bottom'
+                  sideOffset={8}>
+                  {value}
+                </Tooltip.Content>
+              </Tooltip.Portal>
+            </Tooltip.Root>
+          </Tooltip.Provider>
         </Slider.Root>
-        <span>{value}</span>
       </form>
     );
   }
