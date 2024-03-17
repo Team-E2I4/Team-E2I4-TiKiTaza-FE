bccec6c748eb9a27349e34bccb0fed0b15d35eaf
dlsxjzld <sinjuk1@naver.com>
2024-03-17 10:14:41 +0900

    [🎨style]: text 사이즈 키우기, 세로축 가운데로 정렬

--- src/pages/GamePage/GameWaitingRoom/GameModeInfo.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameModeInfo.tsx
@@ -6,19 +6,19 @@ interface GameModeInfoProps {
 }
 const GAME_MODE = {
   SENTENCE: '문장',
-  CODE: '코드',
+  CODE: '코딩',
   WORD: '단어',
 };
 const GAME_MODE_INFO = {
   SENTENCE: `문장을 신속하고 정확하게 입력하여 경주에서 승리하세요!`,
-  CODE: `코드를 신속하고 정확하게 입력하여 경주에서 승리하세요! 아무것도 입력할 것이 안 보인다면 줄 넘김이라 엔터키를 입력하면 됩니다!`,
+  CODE: `코드를 신속하고 정확하게 입력하여 경주에서 승리하세요!`,
   WORD: '단어를 신속하고 정확하게 입력하여 경주에서 승리하세요!',
 };
 
 const GameModeInfo = ({ children, gameMode }: GameModeInfoProps) => {
   return (
-    <div className='w-[69.3rem] h-[10rem] flex bg-beige-100 shadow-md shadow-black/50 rounded-[2.5rem]'>
-      <p className='w-[80%] my-[2.5rem] ml-[2rem] overflow-y-auto text-left'>
+    <div className='w-[69.3rem] h-[10rem] flex items-center bg-beige-100 shadow-md shadow-black/50 rounded-[2.5rem]'>
+      <p className='w-[80%] ml-[2rem] overflow-y-auto text-left text-[2rem]'>
         <strong>{GAME_MODE[gameMode]} 모드입니다. </strong>
         {GAME_MODE_INFO[gameMode]}
       </p>
