1c25cd802d483b4c130151ca113beb2ea514d109
loevray <rei980213@gmail.com>
2024-03-18 12:59:46 +0900

    [🎨style]: 문장게임 텍스트 크기 변경 및 input컴포넌트 props 변경

--- src/common/Input/Input.tsx
+++ src/common/Input/Input.tsx
@@ -6,16 +6,16 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 const Input = forwardRef(
   (
-    { whSize = `w-[70rem] h-[4.5rem]`, isError, ...props }: InputProps,
+    { isError, className, ...props }: InputProps,
     ref?: ForwardedRef<HTMLInputElement>
   ) => {
     return (
       <>
         <input
           ref={ref}
-          className={`${whSize} flex items-center pl-[1.75rem] rounded-2xl
+          className={`w-[70rem] h-[4.5rem] flex items-center pl-[1.75rem] rounded-2xl
 bg-white border-2 ${isError ? 'border-red-500' : 'border-green-100'} my-4
-outline-0 text-gray-300 tracking-wider box-border`}
+outline-0 text-gray-300 tracking-wider box-border ${className}`}
           {...props}
         />
       </>
