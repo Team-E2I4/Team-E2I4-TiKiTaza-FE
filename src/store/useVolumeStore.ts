2cb1b1d7ffce73a2ae3fa73a616b1b505f63d4e7
Baek <woonn97@gmail.com>
2024-03-18 23:25:45 +0900

    [✨feat]: zustand persist 적용

--- src/store/useVolumeStore.ts
+++ src/store/useVolumeStore.ts
@@ -1,13 +1,22 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 import { VolumeType } from '@/common/Header/Header';
 
 export interface I_VolumeStore {
   volume: { bgm: VolumeType; volumeSize: number };
   setVolume: (newVolume: { bgm: VolumeType; volumeSize: number }) => void;
 }
-const useVolumeStore = create<I_VolumeStore>((set) => ({
-  volume: { bgm: 'pause', volumeSize: 50 },
-  setVolume: (newVolume) => set({ volume: newVolume }),
-}));
+const useVolumeStore = create(
+  persist<I_VolumeStore>(
+    (set) => ({
+      volume: { bgm: 'pause', volumeSize: 50 },
+      setVolume: (newVolume) => set({ volume: newVolume }),
+    }),
+    {
+      name: 'volume-storage', // 스토리지에 저장될 때 사용될 키 이름
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
 
 export default useVolumeStore;
