3e7dde9c17494b5ff5368424465c5080b12888ba
yejinleee <yejin9487@gmail.com>
2024-03-18 15:42:07 +0900

    [🎨style]: 트랙 점선 추가 및 파일 분리

---
+++ src/pages/GamePage/common/TrackLine.tsx
@@ -0,0 +1,10 @@
+const TrackLine = () => {
+  return (
+    <>
+      <div className='absolute w-[110rem] h-full rounded-[10rem] border-2 border-black'></div>
+      <div className='absolute w-[105rem] h-[calc(100%-5rem)] border-dashed rounded-[7.5rem] border-[1px] border-gray-300'></div>
+      <div className='absolute w-[100rem] h-[calc(100%-10rem)] rounded-[5rem] border-2 border-black'></div>
+    </>
+  );
+};
+export default TrackLine;
