e8d1252c922842684063141e6fbbe9d14d273192
dlsxjzld <sinjuk1@naver.com>
2024-03-19 00:08:20 +0900

    [🎨style]: 시작 버튼 style 변경

--- src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
@@ -27,9 +27,19 @@ const GameReadyAndStart = ({
         onClick={() => {
           isAllUserReady && handlePubStartGame();
         }}
-        className={`w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] transition-all duration-300 shadow-md shadow-black/50 rounded-[2.5rem]
-        ${isAllUserReady ? `bg-coral-100` : `bg-coral-50 opacity-[0.5] cursor-not-allowed`}
+        className={`w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] transition-all duration-300 shadow-md shadow-black/50 relative
+overflow-hidden
+bg-coral-50
+        ${isAllUserReady ? ` hover:bg-coral-100` : `opacity-[0.5] cursor-not-allowed`}
         `}>
+        {isAllUserReady && (
+          <>
+            <span className='absolute block top-0 left-0 w-full h-[0.3rem] bg-gradient-to-l from-coral-100 animate-neonTop'></span>
+            <span className='absolute block top-[-100%] right-0 w-[0.3rem] h-full bg-gradient-to-t from-coral-100 animate-neonRight'></span>
+            <span className='absolute block bottom-0 right-0 w-full h-[0.3rem] bg-gradient-to-r from-coral-100 animate-neonBottom'></span>
+            <span className='absolute block bottom-[-100%] left-0 w-[0.3rem] h-full bg-gradient-to-b from-coral-100 animate-neonLeft'></span>
+          </>
+        )}
         시작
       </button>
     );
