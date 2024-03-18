aceb5cbe1dac5b16e9ae5dbbfa05da2e4ca13c3e
yejinleee <yejin9487@gmail.com>
2024-03-18 20:43:28 +0900

    [🎨style]: 화면 세로 사이즈 지정

--- src/common/Layout/BodyLayout.tsx
+++ src/common/Layout/BodyLayout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const BodyLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='w-layout-w px-[2rem] py-[2rem] mt-[1rem] flex flex-col bg-bodyLayoutBgColor rounded-[1rem]'>
+    <div className='w-layout-w h-[68.5rem] box-content px-[2rem] py-[2rem] mt-[1rem] flex flex-col bg-bodyLayoutBgColor rounded-[1rem]'>
       {children}
     </div>
   );
