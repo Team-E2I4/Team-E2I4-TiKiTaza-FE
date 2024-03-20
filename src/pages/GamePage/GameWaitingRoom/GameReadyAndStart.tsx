ace6b5124c4f1752f33aa502c17d367f42122b1a
yejinleee <yejin9487@gmail.com>
2024-03-20 10:42:46 +0900

    [🎨style]: 준비/시작 버튼 스타일링

--- src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameReadyAndStart.tsx
@@ -33,7 +33,7 @@ const GameReadyAndStart = ({
         onClick={() => {
           isAllUserReady && handlePubStartGame();
         }}
-        className={`w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] transition-all duration-300 shadow-md shadow-black/50 relative
+        className={`font-[Giants-Inline] w-[24.1rem] h-[10rem] flex justify-center items-center text-[3rem] transition-all duration-300 shadow-md shadow-black/50 relative
 overflow-hidden
 bg-coral-50
         ${isAllUserReady ? ` hover:bg-coral-100` : `opacity-[0.5] cursor-not-allowed`}
@@ -55,7 +55,7 @@ bg-coral-50
       onClick={() => {
         handlePubReadyGame();
       }}
-      className={`group w-[24.1rem] h-[10rem] flex justify-center items-center text-[2rem] ${isUserReady ? 'bg-coral-100' : 'bg-coral-50'}
+      className={`font-[Giants-Inline] group w-[24.1rem] h-[10rem] flex justify-center items-center text-[3rem] ${isUserReady ? 'bg-coral-100' : 'bg-coral-50'}
         relative overflow-hidden
         shadow-md shadow-black/50 z-[0] 
         `}>
@@ -64,7 +64,7 @@ bg-coral-50
             bottom-0 left-0 border-t-transparent border-r-transparent border-b-transparent border-l-coral-100 group-hover:border-y-[10rem] group-hover:border-x-[calc(25.3rem*1.05)]'></span>
       <span
         className='absolute box-border transition-all ease-in-out duration-500 z-[-1] w-0 h-0 border-0 border-solid rotate-[360deg] 
-      top-0 right-0 border-t-transparent border-r-coral-100 border-b-transparent border-l-transparent group-hover:border-y-[10rem] group-hover:border-x-[calc(25.3rem*1.05)]'></span>
+      top-0 right-0 border-t-transparent border-r-coral-100 border-b-transparent border-l-transparent group-hover:border-y-[10rem] group-hover:border-x-[calc(25.3rem*1.05)] font-[Giants-Inline]'></span>
       준비
     </button>
   );
