acae27b060c6f527d6992d1c0e85a46f8c4b320e
dlsxjzld <sinjuk1@naver.com>
2024-03-20 13:49:49 +0900

    [🎨style]: GameRoomInfo에 maxRound 추가

--- src/pages/GamePage/GameWaitingRoom/GameRoomInfo.tsx
+++ src/pages/GamePage/GameWaitingRoom/GameRoomInfo.tsx
@@ -10,16 +10,19 @@ const GameRoomInfo = ({
 }) => {
   return (
     <div className='w-[105rem] h-[7.1rem] text-[2rem] flex bg-beige-100 rounded-[2.5rem] shadow-md shadow-black/50 items-center '>
-      <div className='flex justify-center items-center w-[25%] h-[70%] border-r border-solid border-black'>
+      <div className='flex justify-center items-center w-[20%] h-[70%] border-r border-solid border-black'>
         No.{roomInfo.id}
       </div>
-      <div className='flex justify-center items-center w-[35%] h-[70%] border-r border-solid border-black'>
+      <div className='flex justify-center items-center w-[25%] h-[70%] border-r border-solid border-black'>
         {roomInfo.title}
       </div>
-      <div className='flex justify-center items-center w-[20%] h-[70%] border-r border-solid border-black'>
+      <div className='flex justify-center items-center w-[15%] h-[70%] border-r border-solid border-black'>
         {roomInfo && GAME_TYPE.get(roomInfo.gameMode)}
       </div>
-      <div className='flex justify-center items-center w-[20%] h-[70%]'>{`${allMembers.length} / ${roomInfo.maxPlayer}`}</div>
+      <div className='flex justify-center items-center w-[20%] h-[70%] border-r border-solid border-black'>
+        {roomInfo && roomInfo.maxRound} 라운드
+      </div>
+      <div className='flex justify-center items-center w-[20%] h-[70%]'>{`${allMembers.length} 명 / ${roomInfo.maxPlayer} 명`}</div>
     </div>
   );
 };
