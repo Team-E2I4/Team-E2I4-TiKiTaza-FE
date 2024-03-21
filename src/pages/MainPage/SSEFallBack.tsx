f522228fff973a38709b67b4a8aa453b02db2af2
loevray <rei980213@gmail.com>
2024-03-21 09:38:17 +0900

    [✨feat]: Spinner컴포넌트 추가 - 게임방 목록 간헐적으로 끊길때마다 스피너 출력

--- src/pages/MainPage/SSEFallBack.tsx
+++ src/pages/MainPage/SSEFallBack.tsx
@@ -1,9 +1,17 @@
+import Spinner from '@/common/Spinner/Spinner';
 import { SSEErrorType } from '@/hooks/useSSE';
 
 const SSEFallBack = ({ error }: { error: SSEErrorType }) => {
+  if (error?.error?.message === 'network error') {
+    return (
+      <div className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 row-start-2 col-start-1 col-span-2 flex items-center justify-center '>
+        <Spinner />
+      </div>
+    );
+  }
   return (
     //원래 방 목록 스타일과 겹침. 중복되는 스타일 수정 필요
-    <div className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 row-start-2 col-start-1 col-span-2'>
+    <div className='bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 row-start-2 col-start-1 col-span-2 flex items-center justify-center '>
       방 목록이 없거나 에러 발생!
       {error?.error?.message && (
         <span>{error.error.message} : 서버가 닫혀있어요</span>
