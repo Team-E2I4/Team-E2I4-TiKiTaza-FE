09de222c46e54d0d621bb99a2e7af39a12f50b12
loevray <rei980213@gmail.com>
2024-03-22 14:36:38 +0900

    [🗑️remove]: vite.config 필요없는 fs임포트 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import mkcert from 'vite-plugin-mkcert';
-import fs from 'fs'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
