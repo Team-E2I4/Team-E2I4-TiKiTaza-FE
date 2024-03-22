83dc5a1051fdf5031d424cb806134f226db7b0cc
Baek <woonn97@gmail.com>
2024-03-22 11:28:15 +0900

    [♻️refactor]: tokenStorage 로직 수정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { DefaultApiFactory } from '@/generated';
 import { BASE_PATH } from '@/generated/base';
-import storageFactory from '@/utils/storageFactory';
 import { getTokenStorage } from './tokenStorage';
 
 async function refreshToken() {
@@ -15,8 +14,7 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const storage = getTokenStorage();
-  const { getItem } = storageFactory(storage);
+  const { getItem } = getTokenStorage();
   if (config.url?.endsWith('reissue')) {
     return config;
   }
@@ -34,8 +32,7 @@ axiosInstance.interceptors.response.use(
     if (errorCode === 'sec-401/02') {
       try {
         const accessToken = await refreshToken();
-        const storage = getTokenStorage();
-        const { setItem } = storageFactory(storage);
+        const { setItem } = getTokenStorage();
         if (accessToken) {
           originRequest.headers = {
             ...originRequest.headers,
@@ -47,8 +44,7 @@ axiosInstance.interceptors.response.use(
         }
       } catch (e) {
         if (e) {
-          const storage = getTokenStorage();
-          const { removeItem } = storageFactory(storage);
+          const { removeItem } = getTokenStorage();
           removeItem('MyToken');
           alert('게스트 로그인은 최대 3시간동안 유지됩니다!');
         }
--- src/apis/tokenStorage.ts
+++ src/apis/tokenStorage.ts
@@ -1,13 +1,9 @@
 import storageFactory from '@/utils/storageFactory';
 
 export const getTokenStorage = () => {
-  const { getItem: localGetItem } = storageFactory(localStorage);
-  const { getItem: sessionGetItem } = storageFactory(sessionStorage);
-  if (localGetItem('MyToken', '')) {
-    return localStorage;
-  } else if (sessionGetItem('MyToken', '')) {
-    return sessionStorage;
-  } else {
-    return localStorage; // 기본값으로 localStorage 설정
+  const { getItem: getSessionItem } = storageFactory(sessionStorage);
+  if (getSessionItem('MyToken', '')) {
+    return storageFactory(sessionStorage);
   }
+  return storageFactory(localStorage);
 };
