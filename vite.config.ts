7cae3d2317805773139621dff7d54194fb5a1a3a
loevray <rei980213@gmail.com>
2024-03-22 13:07:35 +0900

    [🔨chore]: GamePage lazy 로딩 적용 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,9 @@ import path from 'path';
 import mkcert from 'vite-plugin-mkcert';
 import fs from 'fs'
 // https://vitejs.dev/config/
-export default defineConfig(({command})=>{
-  return {plugins: [react(), mkcert(),{
-    name: 'build-script',
-        buildStart() {
-          if (command === 'build') {
-            const isDevelopment = process.env.NODE_ENV === 'development';
-            const packageManager = isDevelopment ? 'yarn@4.1.0' : undefined;
-
-            const packageJsonPath = './package.json';
-            const packageJson = require(packageJsonPath);
-
-            packageJson.packageManager = packageManager;
-
-            // 변경된 package.json 파일을 저장합니다.
-            fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-          }}
-  }],
+export default defineConfig({
+  plugins: [react(), mkcert()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },}
-});
+  }}
+);
