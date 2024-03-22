b63405c0b1ae0d4b879f5eeada65af8488f9b11c
loevray <rei980213@gmail.com>
2024-03-22 21:08:00 +0900

    [🔨chore]: vite에서 지원하는 split vendor chunk 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), mkcert(), splitVendorChunkPlugin()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   }}
