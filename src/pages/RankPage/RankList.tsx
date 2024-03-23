b4277a82295297f52d3df37fe241c9f0ca9208a3
yejinleee <yejin9487@gmail.com>
2024-03-23 21:51:12 +0900

    [🎨style]: 랭킹 화면 넘칠시 scroll

--- src/pages/RankPage/RankList.tsx
+++ src/pages/RankPage/RankList.tsx
@@ -15,19 +15,21 @@ const RankList = ({ convertedRankData }: RankListProps) => {
   }, [convertedRankData]);
 
   return (
-    <div className='flex flex-col rounded-2xl overflow-hidden w-[100rem]'>
+    <div className='flex flex-col rounded-2xl w-[80rem] max-h-[50rem] '>
       <div className='flex gap-2 justify-center font-bold font-[Giants-Inline] text-5xl py-4 px-4 border-b border-gray-200'>
         <span className='flex-1 text-center'>순위</span>
         <span className='flex-1 text-center'>닉네임</span>
         <span className='flex-1 text-center'>점수</span>
       </div>
-      {sortedConvertedRankData.map((rank, index) => (
-        <RankItem
-          key={index}
-          rank={rank}
-          index={index}
-        />
-      ))}
+      <div className='overflow-y-auto'>
+        {sortedConvertedRankData.map((rank, index) => (
+          <RankItem
+            key={index}
+            rank={rank}
+            index={index}
+          />
+        ))}
+      </div>
     </div>
   );
 };
