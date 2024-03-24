a4b57f1693badaf245fa6409f40dacfb67181818
loevray <rei980213@gmail.com>
2024-03-24 17:32:26 +0900

    [🎨style]: 게임 목록에 게임중, 대기중 표시 추가 - 방 목록 클릭 후 에러메시지 발생 시 Toast알림 추가

--- src/pages/MainPage/GameRoonListItem.tsx
+++ src/pages/MainPage/GameRoonListItem.tsx
@@ -23,6 +23,7 @@ const GameRoomListItem = ({
   maxPlayer,
   currentPlayer,
   isPrivate,
+  isPlaying,
   setIsOpen,
 }: GameRoomListItemProps) => {
   const { setRoomId } = useRoomInfoStore();
@@ -44,18 +45,22 @@ const GameRoomListItem = ({
             ? () => setIsOpen(true)
             : () => mutateEnterGameRoom({ roomId: id })
         }
-        className='h-[5rem] flex items-center shrink-0 w-full py-[1rem] bg-gray-10 cursor-pointer
+        className={`${isPlaying ? 'opacity-50 cursor-not-allowed' : 'opacity-100 cursor-pointer'} h-[5rem] flex items-center shrink-0 w-full py-[1rem] bg-gray-10 
       [clip-path:polygon(97%_0%,100%_50%,97%_100%,0%_100%,0%_0%)]
-      hover:bg-myGradient hover:animate-gameRoomList'>
+      hover:bg-myGradient hover:animate-gameRoomList`}>
         <span className='text-center truncate flex-1'>{`No.${id}`}</span>
         <Divider
           orientation='vertical'
           className='border-gray-200'
         />
-        <div className='flex-[4_0_0] truncate flex justify-center'>
+        <div className='flex-[4_0_0] truncate flex justify-center items-center'>
           {isPrivate && <LockClosedIcon className='size-[2rem]' />}
           <span className='text-center truncate max-w-[60%] px-[1rem]'>
             {title}
+          </span>
+          <span
+            className={`text-white font-semibold rounded-[0.5rem] px-[0.4rem] ${isPlaying ? 'bg-red-500' : 'bg-green-100'}`}>
+            {isPlaying ? '게임 중' : '대기 중'}
           </span>
         </div>
         <Divider
