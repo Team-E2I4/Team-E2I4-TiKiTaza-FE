d64a60281f04e86bccbef316b55e33d347dd2e32
loevray <rei980213@gmail.com>
2024-04-17 21:42:04 +0900

    [♻️refactor]: 메인페이지 내부 사용중 컴포넌트 리턴문 최상단 article을 메인페이지로 이동

--- src/pages/MainPage/UserList.tsx
+++ src/pages/MainPage/UserList.tsx
@@ -8,7 +8,7 @@ interface UserListProps {
 
 const UserList = ({ userList, myId }: UserListProps) => {
   return (
-    <article className='flex flex-col gap-[1rem] p-[2rem] bg-white rounded-[0.5rem] border-solid border-[0.3rem] border-green-100 h-[40rem] w-full'>
+    <>
       <h3>현재 접속자</h3>
       <ul className='flex-1 rounded-[0.5rem] bg-green-70 p-[2rem] overflow-y-auto'>
         {userList ? (
@@ -23,7 +23,7 @@ const UserList = ({ userList, myId }: UserListProps) => {
           <span>접속중인 유저가 없습니다.</span>
         )}
       </ul>
-    </article>
+    </>
   );
 };
 
