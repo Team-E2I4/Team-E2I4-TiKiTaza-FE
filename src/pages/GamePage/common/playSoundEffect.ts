e61756468e26c0e1439f7a4a2e9be1a412380507
yejinleee <yejin9487@gmail.com>
2024-04-23 10:27:17 +0900

    [💡comment] : 주석 삭제

--- src/pages/GamePage/common/playSoundEffect.ts
+++ src/pages/GamePage/common/playSoundEffect.ts
@@ -15,12 +15,3 @@ const playSoundEffect = async (type: SoundEffectType) => {
   return audio;
 };
 export default playSoundEffect;
-
-// import countDown from '@/assets/audio/countDown.mp3';
-// import soundEffect from '@/assets/audio/soundEffect.mp3';
-
-// const playSoundEffect = (type: 'SCORE' | 'COUNTDOWN') => {
-//   const src = type === 'SCORE' ? soundEffect : countDown;
-//   return new Audio(src);
-// };
-// export default playSoundEffect;
